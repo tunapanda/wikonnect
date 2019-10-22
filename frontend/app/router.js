67e7748f2e6875ec8fab3bfde746a842d25498e8
mrlarso <larson.mick@gmail.com>
2019-10-22 13:29:07 +0300

    user model, login/signup routes

--- frontend/app/router.js
+++ frontend/app/router.js
@@ -7,4 +7,6 @@ export default class Router extends EmberRouter {
 }
 
 Router.map(function() {
+  this.route('signup');
+  this.route('login');
 });
---
+++ frontend/app/routes/signup.js
@@ -0,0 +1,4 @@
+import Route from '@ember/routing/route';
+
+export default class SignupRoute extends Route {
+}
