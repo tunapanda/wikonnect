1d3d4611588e620045c695e9d088017f6df09ebe
Moses Okemwa <okemwamoses@gmail.com>
2019-10-24 22:30:52 +0300

    Auth tests using Red/Green/Refactor cycle

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,6 @@
 const Router = require('koa-router');
 const User = require('../models/user');
-const validateNewUser = require('../middleware/validateNewUser')
+const validateAuthRoutes = require('../middleware/validateAuthRoutes')
 const bcrypt = require('bcrypt');
 const authenticate = require('../middleware/authenticate');
 const jsonwebtoken = require('jsonwebtoken');
@@ -14,31 +14,25 @@ const router = new Router({
 /**
  * You can register with:
  * curl -X POST -d 'user[email]=kenya@tunapanda.org&user[username]=mountkenya&user[hash]=mountkenya' http://localhost:3000/api/v1/auth/register
+ * curl -X POST -d '{ "user": { "username": "okemwa", "hash": "okemwaokewamaokemwa", "email":"moses@okemwa.org" } }' http://localhost:3000/api/v1/auth/register
  */
-router.post('/register', authenticate.getUserByUsername, async ctx => {
+router.post('/register', validateAuthRoutes.validateNewUser, authenticate.getUserByUsername, async ctx => {
     ctx.request.body.user.username = ctx.request.body.user.username.toLowerCase();
     ctx.request.body.user.hash = await bcrypt.hash(ctx.request.body.user.hash, 5);
-    // ctx.request.body.lastip = ctx.req.socket.remoteAddress;
     let newUser = ctx.request.body.user;
     const user = await User.query().insertAndFetch(newUser);
-    ctx.status = 201;
+    ctx.status = 200;
     ctx.body = { user };
 });
 
 /**
  * You can login with:
  * curl -X POST -d 'user[username]=mountkenya&user[hash]=mountkenya' http://localhost:3000/api/v1/auth/login
+ * curl -X POST -d '{ "user": { "username": "okemwa", "hash": "okemwaokewamaokemwa" } }' http://localhost:3000/api/v1/auth/login
  */
 
-router.post('/login', async ctx => {
+router.post('/login', validateAuthRoutes.validateUserLogin, async ctx => {
     let user = await User.query().where("username", ctx.request.body.user.username);
-    if (!user) {
-        ctx.status = 401;
-        ctx.body = {
-            error: "bad username"
-        }
-        return;
-    }
 
     const hashPassword = user[0].hash;
     const userInfoWithoutPassword = delete user[0].hash;
