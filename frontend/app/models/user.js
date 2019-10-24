13d45eb68ee68b553aa48b99aeba1fb1c18eeb72
mrlarso <larson.mick@gmail.com>
2019-10-24 11:03:43 +0300

    registration component

--- frontend/app/models/user.js
+++ frontend/app/models/user.js
@@ -1,6 +1,15 @@
-import DS from 'ember-data';
-const { Model } = DS;
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr'
 
 export default class UserModel extends Model {
+
+    @attr()
+    username
+
+    @attr()
+    password
+
+    @attr()
+    email
 
 }
