4de17f948b06b598b4ac5e7bb3b6fc690d92f178
Jake Lee Kennedy <jlkennedy127@gmail.com>
2019-11-01 14:13:01 +0300

    ember app routes

--- frontend/app/router.js
+++ frontend/app/router.js
@@ -2,11 +2,23 @@ import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 
 export default class Router extends EmberRouter {
-    location = config.locationType;
-    rootURL = config.rootURL;
+  location = config.locationType;
+  rootURL = config.rootURL;
 }
 
 Router.map(function () {
-    this.route('signup');
-    this.route('login');
+  this.route('signup');
+  this.route('login');
+  this.route('courses', { path: '/course/:id' }, function () {
+    this.route('modules', { path: '/module/:id' }, function () {
+      this.route('lessons', { path: '/lesson/:id' }, function () {
+        this.route('chapters', { path: '/chapter/:id' });
+      });
+    });
+  });
+  this.route('profile', { path: '/profile/:id' }, function () {
+    this.route('settings');
+  });
+  this.route('search');
+  this.route('about');
 });
