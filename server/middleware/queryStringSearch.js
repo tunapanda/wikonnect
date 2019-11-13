5d30a81ae1e2feec8930d470f62b2746277be254
Moses Okemwa <okemwamoses@gmail.com>
2019-11-13 12:04:18 +0300

    extended middleware to support multiple query

--- server/middleware/queryStringSearch.js
+++ server/middleware/queryStringSearch.js
@@ -7,20 +7,21 @@ async function queryStringSearch(ctx, next) {
   if (isEmpty(ctx.query)) {
     const key = 'id';
     const value = ctx.params.id;
+    const sort = 'status';
 
-    ctx.query = { key, value };
+    ctx.query = { key, value, sort };
   } else {
     const keys = Object.keys(ctx.query);
     const values = Object.values(ctx.query);
 
     const key = keys[0];
     const value = values[0];
+    const sort = values[1] ? values[1] : 'status';
 
-    ctx.query = { key, value };
+    ctx.query = { key, value, sort };
 
   }
   await next();
 }
 
 module.exports = queryStringSearch;
-
