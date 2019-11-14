2fe1d96a40ce5dcb7b00d662bf40d298924a5f6f
Mick <mick@tunapanda.org>
2019-11-14 16:56:24 +0300

    DB seeds for dummy course

--- frontend/app/models/module.js
+++ frontend/app/models/module.js
@@ -2,10 +2,10 @@ import DS from 'ember-data';
 const { Model, attr, belongsTo, hasMany } = DS;
 
 export default class ModuleModel extends Model {
-  @attr() name;
-  @attr() slug;
-  @attr() description;
-  @attr() status;
-  @belongsTo('user') creator;
-  @hasMany('lesson') lessons;
+    @attr() name;
+    @attr() slug;
+    @attr() description;
+    @attr() status;
+    @belongsTo('user') creator;
+    @hasMany('lesson') lessons;
 }
