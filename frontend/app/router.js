c8fb70f09d2f5e55f84acebddc191d145bf6a477
Mick <mick@tunapanda.org>
2019-11-15 09:06:05 +0300

    fixed linting errors

--- frontend/app/router.js
+++ frontend/app/router.js
@@ -2,24 +2,24 @@ import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 
 export default class Router extends EmberRouter {
-    location = config.locationType;
-    rootURL = config.rootURL;
+  location = config.locationType;
+  rootURL = config.rootURL;
 }
 
 Router.map(function () {
-    this.route('signup');
-    this.route('login');
-    this.route('home');
-    this.route('course', { path: 'course/:course_id' }, function () {
-        this.route('module', { path: 'module/:module_id' }, function () {
-            this.route('lesson', { path: '/lesson/:lesson_id' }, function () {
-                this.route('chapters', { path: '/chapter/:chapter_id' });
-            });
-        });
+  this.route('signup');
+  this.route('login');
+  this.route('home');
+  this.route('course', { path: 'course/:course_id' }, function () {
+    this.route('module', { path: 'module/:module_id' }, function () {
+      this.route('lesson', { path: '/lesson/:lesson_id' }, function () {
+        this.route('chapters', { path: '/chapter/:chapter_id' });
+      });
     });
-    this.route('profile', { path: '/profile/:profile_id' }, function () {
-        this.route('settings');
-    });
-    this.route('search');
-    this.route('about');
+  });
+  this.route('profile', { path: '/profile/:profile_id' }, function () {
+    this.route('settings');
+  });
+  this.route('search');
+  this.route('about');
 });
