88c36875ed524fc01ff2ffcce55bd296772df7f3
Mick <mick@tunapanda.org>
2019-11-15 10:09:25 +0300

    more linting errors

--- frontend/app/routes/home.js
+++ frontend/app/routes/home.js
@@ -2,7 +2,7 @@ import Route from '@ember/routing/route';
 
 export default class HomeRoute extends Route {
 
-    model() {
-        return this.store.findAll('course')
-    }
+  model() {
+    return this.store.findAll('course')
+  }
 }
