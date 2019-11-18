842165645575e7fb4ffbb35bfd67e0f14727d618
Proverbial Ninja <kariuki.marima@gmail.com>
2019-11-18 14:36:42 +0300

    Closes #16

--- frontend/app/routes/course/index.js
+++ frontend/app/routes/course/index.js
@@ -1,5 +1,8 @@
 import Route from '@ember/routing/route';
 
 export default class CourseIndexRoute extends Route {
+  async model() {
 
+
+  }
 }
--- frontend/app/routes/course/module/index.js
+++ frontend/app/routes/course/module/index.js
@@ -1,4 +1,5 @@
 import Route from '@ember/routing/route';
 
 export default class CoursesModulesIndexRoute extends Route {
+
 }
