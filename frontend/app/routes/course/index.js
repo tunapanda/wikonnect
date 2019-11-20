2ae24673e6580c3dd02d131696bffcfb32164907
Proverbial Ninja <kariuki.marima@gmail.com>
2019-11-20 12:47:54 +0300

    demoved model

--- frontend/app/routes/course/index.js
+++ frontend/app/routes/course/index.js
@@ -1,8 +1,8 @@
 import Route from '@ember/routing/route';
 
 export default class CourseIndexRoute extends Route {
-  async model() {
-
-    return this.store.findRecord('course', "diglit", { include: 'modules' });
-  }
+  // async model(params) {
+  //   console.log(params);
+  //   return this.store.findRecord('course', "diglit", { include: 'modules' });
+  // }
 }
--- frontend/app/routes/course/module/index.js
+++ frontend/app/routes/course/module/index.js
@@ -2,8 +2,5 @@ import Route from '@ember/routing/route';
 
 export default class CoursesModulesIndexRoute extends Route {
 
-  model(params) {
-    console.log(params);
-    return this.store.findRecord('module', "diglit1", { include: 'lesson' });
-  }
+
 }
