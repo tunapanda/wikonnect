025996e31fd52c310a501e84fc78f1b3071c0548
Proverbial Ninja <kariuki.marima@gmail.com>
2019-11-20 11:21:26 +0300

    lessons and module items with hardcoded params

--- frontend/app/routes/course/index.js
+++ frontend/app/routes/course/index.js
@@ -3,6 +3,6 @@ import Route from '@ember/routing/route';
 export default class CourseIndexRoute extends Route {
   async model() {
 
-
+    return this.store.findRecord('course', "diglit", { include: 'modules' });
   }
 }
