b0d0a3147951506cd80035823e95a2fed16a3582
Jake Lee Kennedy <jlkennedy127@gmail.com>
2019-11-22 09:50:36 +0000

    search test indexing with course model

--- server/models/course.js
+++ server/models/course.js
@@ -1,14 +1,14 @@
 const Model = require('./_model');
 const knex = require('../db/db');
 const modelSchema = require('../db/json_schema/modelSchema');
-
+const search = require('../utils/search');
 
 class Course extends Model {
   static get tableName() {
     return 'courses';
   }
 
-  static get jsonSchema(){
+  static get jsonSchema() {
     return modelSchema;
   }
 
@@ -27,6 +27,36 @@ class Course extends Model {
         }
       }
     };
+  }
+
+  async $afterInsert() {
+    return search.index({
+      index: search.indexName,
+      id: this.id,
+      body: {
+        model: 'course',
+        name: this.name,
+        description: this.description,
+        status: this.status,
+        created_at: this.createdAt,
+        modified_at: this.modifiedAt
+      }
+    });
+  }
+
+  async $afterUpdate() {
+    return search.update({
+      index: search.indexName,
+      id: this.id,
+      body: {
+        model: 'course',
+        name: this.name,
+        description: this.description,
+        status: this.status,
+        created_at: this.createdAt,
+        modified_at: this.modifiedAt
+      }
+    });
   }
 }
 
