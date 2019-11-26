f718a0eed546f1db9340105e3ea2bdf58a9d0d9f
Mick <mick@tunapanda.org>
2019-11-26 11:38:20 +0300

    minor changes

--- frontend/app/routes/course/index.js
+++ frontend/app/routes/course/index.js
@@ -1,20 +1,7 @@
 import Route from '@ember/routing/route';
 
 export default class CourseIndexRoute extends Route {
-  // async model(params) {
-  //   console.log(params);
-  //   return this.store.findRecord('course', "diglit", { include: 'modules' });
-  // }
-
-  // breadCrumb() {
-  //   return {
-  //     title: 'Animals',
-  //     path: 'foo'
-  //   };
-  // }
   model(params) {
-    console.log("course");
-    console.log(params);
     return this.store.findBySlug('course', params.course_slug);
   }
 }
