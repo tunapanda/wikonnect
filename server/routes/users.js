e58dedb01418ab096ff7a49e3aaa2b872a671137
Mick <mick@tunapanda.org>
2019-11-26 10:43:02 +0300

    linting errors

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,7 +3,6 @@ const User = require('../models/user');
 const validateAuthRoutes = require('../middleware/validation/validateAuthRoutes');
 const bcrypt = require('bcrypt');
 const getUserByUsername = require('../middleware/authenticate');
-const { requireAuth } = require('../middleware/auth');
 
 
 const router = new Router({
