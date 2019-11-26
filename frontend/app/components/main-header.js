48270c90b1ea94ddafb2f383f7f4937c68f2e39a
Proverbial Ninja <kariuki.marima@gmail.com>
2019-11-26 09:33:53 +0300

    linting

--- frontend/app/components/main-header.js
+++ frontend/app/components/main-header.js
@@ -1,6 +1,6 @@
 import Component from '@ember/component';
 import { inject } from '@ember/service';
-import { inject as service } from '@ember/service';
+// import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
 
 
