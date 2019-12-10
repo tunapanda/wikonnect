34f6a12e60efb3220af1ce5e2c306ddaaa59d4ad
Moses Okemwa <okemwamoses@gmail.com>
2019-12-10 12:53:42 +0300

    save and aedit data in join tables

--- server/routes/courses.js
+++ server/routes/courses.js
@@ -25,6 +25,18 @@ async function returnType(parent) {
   }
 }
 
+async function insertType(model, collection, course_id) {
+  for (let index = 0; index < collection.length; index++) {
+    const element = collection[index];
+    let data = {
+      'learning_path_id': element,
+      'course_id': course_id
+    };
+    await knex(model).insert(data);
+  }
+}
+
+
 router.get('/:id', async ctx => {
   const course = await Course.query().findById(ctx.params.id).eager('modules(selectNameAndId)');
   if (!course) {
@@ -37,7 +49,7 @@ router.get('/:id', async ctx => {
 });
 
 router.get('/', async ctx => {
-  try{
+  try {
     const course = await Course.query().where(ctx.query).eager('modules(selectNameAndId)');
     returnType(course);
 
@@ -51,28 +63,36 @@ router.get('/', async ctx => {
 
 router.post('/', validatePostData, async ctx => {
   let { learning_path_id, ...newCourse } = ctx.request.body.courses;
-
   const course = await Course.query().insertAndFetch(newCourse);
 
-  await knex('learning_path_courses').insert([
-    {
-      learning_path_id: learning_path_id,
-      course_id: course.id
-    }]);
-
+  insertType('learning_path_courses', learning_path_id, course.id);
 
   ctx.assert(course, 401, 'Something went wrong');
-
   ctx.status = 201;
-
   ctx.body = { newCourse };
-
 });
-router.put('/:id', async ctx => {
-  const course = await Course.query().patchAndFetchById(ctx.params.id, ctx.request.body);
 
-  if (!course) {
-    ctx.throw(400, 'That course does not exist');
+router.put('/:id', async ctx => {
+  let { learning_path_id, ...newCourse } = ctx.request.body.courses;
+  const course = await Course.query().patchAndFetchById(ctx.params.id, newCourse);
+
+  ctx.assert(course, 400, 'That course does not exist');
+
+  const rookie = await knex('learning_path_courses').where('courses_id', course.id);
+
+  if (!learning_path_id == undefined) {
+    let put_data = [];
+    for (let index = 0; index < learning_path_id.length; index++) {
+      put_data.push(learning_path_id[index]);
+    }
+
+    for (let index = 0; index < rookie.length; index++) {
+      const rook = rookie[index].module_id;
+      if (rook != put_data[index]) {
+        await knex('learning_path_courses').where({ 'courses_id': course.id, 'module_id': rook }).del();
+        await knex('learning_path_courses').insert({ 'courses_id': course.id, 'module_id': put_data[index] });
+      }
+    }
   }
 
   ctx.status = 201;
