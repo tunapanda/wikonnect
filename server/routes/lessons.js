4d9e3128885af4fae5828f43f972e095f9e511e0
Moses Okemwa <okemwamoses@gmail.com>
2019-12-10 10:17:06 +0300

    handles update for joining tables

--- server/routes/lessons.js
+++ server/routes/lessons.js
@@ -24,6 +24,17 @@ async function returnType(parent) {
   }
 }
 
+async function insertType(model, collection, model_id) {
+  for (let index = 0; index < collection.length; index++) {
+    const element = collection[index];
+    let data = {
+      'module_id': element,
+      'lesson_id': model_id
+    };
+    await knex(model).insert(data);
+  }
+}
+
 router.get('/:id', async ctx => {
   const lesson = await Lesson.query().findById(ctx.params.id).eager('chapters(selectNameAndId)');
 
@@ -49,29 +60,43 @@ router.get('/', async ctx => {
 });
 
 router.post('/', validatePostData, async ctx => {
-  let { module_id, ...newLesson} = ctx.request.body.lessons;
+  let { module_id, ...newLesson } = ctx.request.body.lessons;
 
-  const lesson = await Lesson.query().insertAndFetch(newLesson);
+  const lesson = await Lesson.query().insertAndFetch(newLesson).eager('chapters(selectNameAndId)');
 
-  await knex('module_lessons').insert([
-    {
-      module_id: module_id,
-      lesson_id: lesson.id
-    }]);
+  insertType('module_lessons', module_id, lesson.id);
 
   ctx.assert(lesson, 401, 'Something went wrong');
 
   ctx.status = 201;
-
   ctx.body = { lesson };
 
 });
+
+
 router.put('/:id', async ctx => {
   const lesson_record = await Lesson.query().findById(ctx.params.id);
   if (!lesson_record) {
     ctx.throw(400, 'That lesson path does not exist');
   }
-  const lesson = await Lesson.query().patchAndFetchById(ctx.params.id, ctx.request.body);
+
+  let { module_id, ...newLesson } = ctx.request.body.lessons;
+  const lesson = await Lesson.query().patchAndFetchById(ctx.params.id, newLesson).eager('chapters(selectNameAndId)');
+  const rookie = await knex('module_lessons').where('lesson_id', lesson.id);
+
+
+  let put_module = [];
+  for (let index = 0; index < module_id.length; index++) {
+    put_module.push(module_id[index]);
+  }
+
+  for (let index = 0; index < rookie.length; index++) {
+    const rook = rookie[index].module_id;
+    if (rook != put_module[index]) {
+      await knex('module_lessons').where({ 'lesson_id': lesson.id, 'module_id': rook }).del();
+      await knex('module_lessons').insert({ 'lesson_id': lesson.id, 'module_id': put_module[index] });
+    }
+  }
 
   ctx.status = 201;
   ctx.body = { lesson };
