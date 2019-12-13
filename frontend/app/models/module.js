b53d0cbb52865d42f557658e7b92c904899d19bd
mrlarso <larson.mick@gmail.com>
2019-12-13 16:12:23 +0300

    Create course module (MVP) (#68)
    * course creation page
    
    * Module/lesson create and edit pages
    
    * update README
    
    * remove package-lock
    
    * fixed linting errors

--- frontend/app/models/module.js
+++ frontend/app/models/module.js
@@ -2,11 +2,11 @@ import DS from 'ember-data';
 const { Model, attr, belongsTo, hasMany } = DS;
 
 export default class ModuleModel extends Model {
-    @attr() name;
-    @attr() slug;
-    @attr() description;
-    @attr() status;
-    @belongsTo('user') creator;
-    @belongsTo('course') course;
-    @hasMany('lesson') lessons;
+  @attr() name;
+  @attr() slug;
+  @attr() description;
+  @attr() status;
+  @belongsTo('user') creator;
+  @hasMany('course') courses;
+  @hasMany('lesson') lessons;
 }
---
+++ frontend/app/serializers/course.js
@@ -0,0 +1,5 @@
+import ApplicationSerializer from './application';
+import DS from 'ember-data';
+
+export default ApplicationSerializer.extend(DS.EmbeddedRecordsMixin, {
+});
