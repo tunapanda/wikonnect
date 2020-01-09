6985a7c2093124b07c2bf3602607ead6fcf7625c
Moses Okemwa <okemwamoses@gmail.com>
2020-01-09 05:25:49 +0300

    missing file

---
+++ server/config/db.example.js
@@ -0,0 +1,20 @@
+module.exports = {
+  test: {
+    host: 'localhost',
+    database: 'swag_test',
+    user: 'swag',
+    password: 'password'
+  },
+  development: {
+    host: 'localhost',
+    database: 'swag_dev',
+    user: 'swag',
+    password: 'password',
+  },
+  production: {
+    host: 'localhost',
+    database: 'swag',
+    user: 'swag',
+    password: 'password'
+  }
+};
