eed3843d7531d5ddf318f7186d4119bd79ce8fc6
Moses Okemwa <okemwamoses@gmail.com>
2020-01-21 09:33:49 +0300

    user enrollments into courses

--- server/routes/enrollments.js
+++ server/routes/enrollments.js
@@ -1,5 +1,6 @@
 const Router = require('koa-router');
 const User = require('../models/user');
+const Enrollments = require('../models/enrollment');
 const { requireAuth } = require('../middleware/permController');
 
 
@@ -10,7 +11,25 @@ const router = new Router({
 });
 
 
+router.post('/', requireAuth, async ctx => {
+  /**
+   * enroll = {
+   *    courses_id => string,
+   *    user_id => string
+   * }
+   */
+  const enrollments = await Enrollments.query().insertAndFetch(ctx.query);
+  ctx.status = 200;
+  ctx.body = { enrollments };
+});
+
 router.get('/', requireAuth, async ctx => {
+  const enrollment = await Enrollments.query().where({ 'user_id': ctx.state.user.data.id});
+  ctx.status = 200;
+  ctx.body = { enrollment };
+});
+
+router.get('/user', requireAuth, async ctx => {
   const user = await User.query().where(ctx.query).eager('enrolledCourses(selectNameAndId)');
   console.log(user[0].enrolledCourses);
 
