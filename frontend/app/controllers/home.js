1d224849ef0e00f0834f269dad347d681e77c072
Proverbial Ninja <kariuki.marima@gmail.com>
2020-02-17 12:34:58 +0300

    qa fixes and some tests

--- frontend/app/controllers/home.js
+++ frontend/app/controllers/home.js
@@ -1,7 +1,10 @@
 import Controller from '@ember/controller';
 import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default class HomeController extends Controller {
+  @service
+  me
 
   colorList = ['54378B', 'F57010', '32A583']
 
