add3d796fd8cd65c1e8991eb5f3283c2e6819a3e
Moses Okemwa <okemwamoses@gmail.com>
2020-02-25 15:56:41 +0300

    docs genereated for frontend

--- doc/api_data.js
+++ doc/api_data.js
@@ -1,33 +1,5 @@
 define({ "api": [
   {
-    "success": {
-      "fields": {
-        "Success 200": [
-          {
-            "group": "Success 200",
-            "optional": false,
-            "field": "varname1",
-            "description": "<p>No type.</p>"
-          },
-          {
-            "group": "Success 200",
-            "type": "String",
-            "optional": false,
-            "field": "varname2",
-            "description": "<p>With type.</p>"
-          }
-        ]
-      }
-    },
-    "type": "",
-    "url": "",
-    "version": "0.0.0",
-    "filename": "server/doc/main.js",
-    "group": "/Users/principal/Documents/projects/tunapanda/swag/server/doc/main.js",
-    "groupTitle": "/Users/principal/Documents/projects/tunapanda/swag/server/doc/main.js",
-    "name": ""
-  },
-  {
     "type": "get",
     "url": "/courses/:id",
     "title": "GET single course.",
--- doc/api_project.js
+++ doc/api_project.js
@@ -17,7 +17,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-02-24T12:01:47.648Z",
+    "time": "2020-02-25T12:55:48.148Z",
     "url": "http://apidocjs.com",
     "version": "0.20.0"
   }
