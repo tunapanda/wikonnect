c9d77cd73f99b026c19491fcb548bad13822d1df
Moses Okemwa <okemwamoses@gmail.com>
2020-03-10 22:21:31 +0300

    ratelimiter config for ci env

--- server/middleware/rateLimiter.js
+++ server/middleware/rateLimiter.js
@@ -12,6 +12,12 @@ const environment = process.env.NODE_ENV || 'development';
 
 
 const env_rate_limiter = {
+  ci: {
+    redis: redisClient,
+    keyPrefix: 'middleware',
+    points: 100, // 100 requests for ctx.ip
+    duration: 1, // per 1 second
+  },
   test: {
     redis: redisClient,
     keyPrefix: 'middlewareTest',
@@ -28,13 +34,15 @@ const env_rate_limiter = {
     duration: 60, // Per 60 seconds,
     blockDuration: 120, // Block duration in store
     inmemoryBlockOnConsumed: 301, // If userId or IP consume >300 points per minute
-    inmemoryBlockDuration: 120,
+    inmemoryBlockDuration: 120
   },
   production: {
-    redis: redisClient,
-    keyPrefix: 'middleware',
-    points: 100, // 100 requests for ctx.ip
-    duration: 1, // per 1 second
+    storeClient: redisClient,
+    points: 300, // Number of points
+    duration: 60, // Per 60 seconds,
+    blockDuration: 120, // Block duration in store
+    inmemoryBlockOnConsumed: 301, // If userId or IP consume >300 points per minute
+    inmemoryBlockDuration: 120,
   }
 };
 
