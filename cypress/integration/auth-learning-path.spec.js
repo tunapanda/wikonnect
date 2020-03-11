1f5e4115c2586ce8a4b35d3043288cbeb83e5817
Proverbial Ninja <kariuki.marima@gmail.com>
2020-03-11 13:12:14 +0300

    learning path tests

---
+++ cypress/integration/auth-learning-path.spec.js
@@ -0,0 +1,37 @@
+describe('Login Page', function () {
+  // it('should login', function () {
+  //   cy.visit('/login');
+
+  //   cy.get('#username input').type('user1');
+  //   cy.get('#password input').type('password1234');
+
+  //   cy.get('.submit').click();
+
+  //   cy.location('pathname').should('eq', '/login');
+
+
+  // });
+
+  it('should flow through authed user learning path', function () {
+
+    cy.visit('/login');
+
+    cy.get('#username input').type('user1');
+    cy.get('#password input').type('wikonnect');
+    cy.get('.submit').click();
+    cy.wait(3000);
+    cy.visit('/courses');
+    cy.contains('A Course 1').click({ force: true })
+    cy.location('pathname').should('eq', '/course/a-course-1');
+    cy.contains("A Module").click({ force: true })
+    cy.location('pathname').should('eq', '/course/module/a-module-1');
+    cy.contains("A Lesson").click({ force: true })
+    cy.location('pathname').should('eq', '/course/module/lesson/a-lesson');
+    cy.contains("A Chapter").click({ force: true })
+    cy.location('pathname').should('eq', '/course/module/lesson/chapter/chapter1');
+
+    cy.contains("A Chapter")
+
+
+  });
+}); 
---
+++ cypress/integration/noauth-learning-path.spec.js
@@ -0,0 +1,21 @@
+describe('Login Page', function () {
+
+
+  it('should flow through unauthenticated user learning path', function () {
+
+
+    cy.visit('/courses');
+    cy.contains('A Course 1').click({ force: true })
+    cy.location('pathname').should('eq', '/course/a-course-1');
+    cy.contains("A Module").click({ force: true })
+    cy.location('pathname').should('eq', '/course/module/a-module-1');
+    cy.contains("A Lesson").click({ force: true })
+    cy.location('pathname').should('eq', '/course/module/lesson/a-lesson');
+    cy.contains("A Chapter").click({ force: true })
+    cy.location('pathname').should('eq', '/course/module/lesson/chapter/chapter1');
+
+    cy.contains("A Chapter")
+
+
+  });
+}); 
