6a5b855fb3a189b47280169abba64af169a4efac
Moses Okemwa <okemwamoses@gmail.com>
2020-03-18 17:29:42 +0300

    verification of email and 404 handler

--- server/server.js
+++ server/server.js
@@ -60,6 +60,27 @@ router.get('/hello', async ctx => {
   ctx.body = { user: 'You have access to view this route' };
 });
 
+app.use(async function pageNotFound(ctx) {
+  // we need to explicitly set 404 here
+  // so that koa doesn't assign 200 on body=
+  ctx.status = 404;
+
+  switch (ctx.accepts('html', 'json')) {
+  case 'html':
+    ctx.type = 'html';
+    ctx.body = '<p>Page Not Found</p>';
+    break;
+  case 'json':
+    ctx.body = {
+      message: 'Page Not Found'
+    };
+    break;
+  default:
+    ctx.type = 'text';
+    ctx.body = 'Page Not Found';
+  }
+});
+
 app.use(router.routes());
 
 module.exports = app;
