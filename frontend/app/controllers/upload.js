428d7b5a581c2dc8a8483e8af8d57a678224c693
Proverbial Ninja <kariuki.marima@gmail.com>
2020-03-31 15:46:41 +0300

    added target status

--- frontend/app/controllers/upload.js
+++ frontend/app/controllers/upload.js
@@ -2,6 +2,8 @@ import Controller from '@ember/controller';
 import { inject } from '@ember/service';
 import { computed, action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+import Uploader from '../utils/uploader';
+
 
 
 
