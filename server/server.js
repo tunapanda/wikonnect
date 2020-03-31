d4c24aebc6e0a2fe22c17143873639ed83019963
Moses Okemwa <okemwamoses@gmail.com>
2020-03-31 10:09:34 +0300

    auto fixed lint issues

--- server/server.js
+++ server/server.js
@@ -61,18 +61,18 @@ if (process.env.NODE_ENv == 'development') {
     // so that koa doesn't assign 200 on body=
     ctx.status = 404;
     switch (ctx.accepts('html', 'json')) {
-      case 'html':
-        ctx.type = 'html';
-        ctx.body = '<p>Page Not Found</p>';
-        break;
-      case 'json':
-        ctx.body = {
-          message: 'Page Not Found'
-        };
-        break;
-      default:
-        ctx.type = 'text';
-        ctx.body = 'Page Not Found';
+    case 'html':
+      ctx.type = 'html';
+      ctx.body = '<p>Page Not Found</p>';
+      break;
+    case 'json':
+      ctx.body = {
+        message: 'Page Not Found'
+      };
+      break;
+    default:
+      ctx.type = 'text';
+      ctx.body = 'Page Not Found';
     }
   });
 }
