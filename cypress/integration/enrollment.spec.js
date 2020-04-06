719e0a8226d2699dd9b794a5c72e40a9d03adea4
Proverbial Ninja <kariuki.marima@gmail.com>
2020-04-06 04:36:44 +0300

    cypress passing

--- cypress/integration/enrollment.spec.js
+++ cypress/integration/enrollment.spec.js
@@ -10,8 +10,8 @@ describe('Enrollments', function () {
     cy.get('.submit').click();
     cy.wait(3000);
     cy.visit('/courses');
-    cy.contains('A Course 2').click({ force: true })
-    cy.location('pathname').should('eq', '/course/a-course-2');
+    cy.contains('A Course 1').click({ force: true })
+    cy.location('pathname').should('eq', '/course/a-course-1');
 
 
 
