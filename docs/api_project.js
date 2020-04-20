134bebcd1a78e3ff42c38c4e48add22d01be0277
Moses Okemwa <okemwamoses@gmail.com>
2020-04-20 15:14:24 +0300

    api docs updated

--- docs/api_project.js
+++ docs/api_project.js
@@ -43,7 +43,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-04-20T11:48:37.035Z",
+    "time": "2020-04-20T12:13:53.697Z",
     "url": "http://apidocjs.com",
     "version": "0.20.0"
   }
