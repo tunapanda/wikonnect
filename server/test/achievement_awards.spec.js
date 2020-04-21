2151e93009cde15bfd2c6ae08ccdf93f073466d3
Moses Okemwa <okemwamoses@gmail.com>
2020-04-22 01:58:39 +0300

    fixed lint issues

--- server/test/achievement_awards.spec.js
+++ server/test/achievement_awards.spec.js
@@ -2,8 +2,6 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../index');
 const tokens = require('./_tokens');
-const knex = require('../db/db');
-
 
 chai.should();
 chai.use(chaiHttp);
