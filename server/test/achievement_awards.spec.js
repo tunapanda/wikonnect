36ff5ded53d2db761ad61af6612f6e49826abc4c
Moses Okemwa <okemwamoses@gmail.com>
2020-04-23 15:55:42 +0300

    embed code added

--- server/test/achievement_awards.spec.js
+++ server/test/achievement_awards.spec.js
@@ -2,6 +2,8 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../index');
 const tokens = require('./_tokens');
+const knex = require('../db/db');
+
 
 chai.should();
 chai.use(chaiHttp);
@@ -9,6 +11,12 @@ chai.use(chaiHttp);
 const achievementAwardsRoute = '/api/v1/achievementAwards/';
 
 describe('Achievement Awards route and function test', () => {
+  before(async () => {
+    await knex.migrate.rollback();
+    await knex.migrate.latest();
+    return knex.seed.run();
+  });
+
   it('Should GET all Achievement Awards with valid token', done => {
     chai
       .request(server)
