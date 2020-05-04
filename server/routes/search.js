5809ef6d8c4514f6d1fa90a24584e80596b7b9ae
Moses Okemwa <okemwamoses@gmail.com>
2020-05-04 22:50:47 +0300

    updated the searchable route with tags in cahpters

--- server/routes/search.js
+++ server/routes/search.js
@@ -32,15 +32,13 @@ const router = new Router({
 
 router.get('/', async ctx => {
   const queryText = ctx.query.q;
-  console.log(queryText);
-
   try {
     const elasticResponse = await search.search({
       index: search.indexName,
       body: {
         query: {
           query_string: {
-            fields: ['name^2', 'description'],
+            fields: ['name^2', 'description', 'tags'],
             query: queryText
           }
         },
