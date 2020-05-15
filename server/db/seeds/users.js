33ec6e8d0e4a0dc94018a02555d047e309bbfa59
Moses Okemwa <okemwamoses@gmail.com>
2020-05-15 12:31:45 +0300

    pravacy settings for user profile closes#255

--- server/db/seeds/users.js
+++ server/db/seeds/users.js
@@ -12,6 +12,7 @@ exports.seed = function (knex) {
           hash: '$2b$10$a.r.mPrpHxwlGh/6jd.RG.QhHu2xHAY3/EzclbPFduSDTnAtV3feu',
           metadata: '{ "customer": "Lily Bush", "items": {"product": "Diaper","qty": 24}}',
           last_ip: '245.19.225.55',
+          private: true,
           last_seen: '2017-12-20 19:17:10',
           created_at: '2017-12-20 19:17:10',
           updated_at: '2017-12-20 19:17:10'
@@ -22,6 +23,7 @@ exports.seed = function (knex) {
           email: 'user2@wikonnect.org',
           hash: '$2b$10$a.r.mPrpHxwlGh/6jd.RG.QhHu2xHAY3/EzclbPFduSDTnAtV3feu',
           last_ip: '245.19.225.55',
+          private: false,
           last_seen: '2017-12-20 19:17:10',
           created_at: '2017-12-20 19:17:10',
           updated_at: '2017-12-20 19:17:10'
@@ -32,6 +34,7 @@ exports.seed = function (knex) {
           email: 'user3@wikonnect.org',
           hash: '$2b$10$a.r.mPrpHxwlGh/6jd.RG.QhHu2xHAY3/EzclbPFduSDTnAtV3feu',
           last_ip: '245.19.225.55',
+          private: false,
           last_seen: '2017-12-20 19:17:10',
           created_at: '2017-12-20 19:17:10',
           updated_at: '2017-12-20 19:17:10'
