dccb9e40fb3e0a6c71652ab0cef4c204f4463428
Moses Okemwa <okemwamoses@gmail.com>
2020-05-21 15:11:26 +0300

    migration nad test files

--- server/db/migrations/20200521145157_chapter_content.js
+++ server/db/migrations/20200521145157_chapter_content.js
@@ -2,14 +2,13 @@
 exports.up = function (knex) {
   return knex.schema
     .table('chapters', table => {
-      table.text('tags');
+      table.text('content_id');
     });
-
 };
 
 exports.down = function (knex) {
   return knex.schema
     .table('chapters', table => {
-      table.dropColumn('tags');
+      table.dropColumn('content_id');
     });
 };
