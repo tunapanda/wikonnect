69946872fce25c6fe98a02dee8b36f82f369df44
IjayAbby <inyangasi4@gmail.com>
2020-05-25 12:26:13 +0300

    email validator code

--- server/routes/users.js
+++ server/routes/users.js
@@ -23,6 +23,31 @@ const router = new Router({
   prefix: '/users'
 });
 
+//validations user.js
+import {
+  validatePresence,
+  validateLength,
+  validateConfirmation,
+  validateFormat
+} from 'ember-changeset-validations/validators';
+import validateCustom from '../validators/custom'; // local validator
+import validatePasswordStrength from '../validators/password-strength'; // local validator
+
+export default {
+  firstName: [
+    validatePresence(true),
+    validateLength({ min: 4 })
+  ],
+  lastName: validatePresence(true),
+  age: validateCustom({ foo: 'bar' }),
+  email: validateFormat({ type: 'email' }),
+  password: [
+    validateLength({ min: 8 }),
+    validatePasswordStrength({ minScore: 80 })
+  ],
+  passwordConfirmation: validateConfirmation({ on: 'password' })
+};
+
 
 async function returnType(parent) {
   try {
