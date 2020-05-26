8b98e20bc1093bcb4d46a7834864f8c44ba9b492
Moses Okemwa <okemwamoses@gmail.com>
2020-05-26 17:30:26 +0300

    chapter seed update

--- server/db/seeds/chapters.js
+++ server/db/seeds/chapters.js
@@ -21,129 +21,17 @@ exports.seed = function (knex) {
           updated_at: '2017-12-20 19:17:10'
         },
         {
-          id: 'chapter2',
-          name: 'A Chapter 2',
-          slug: 'a-chapter-2',
-          description: 'An H5P Chapter.',
+          id: 'coronavirus',
+          name: 'How Much Do You Really Know About Coronavirus?',
+          slug: 'coronavirus',
+          description: 'Should you wear a face mask? Can you catch the virus from your dog? Put your Covid-19 knowledge to the test. By Jason Gale',
           status: 'published',
-          lesson_id: 'lesson1',
           creator_id: 'user1',
           content_type: 'h5p',
-          content_uri: '/uploads/h5p/chapter2',
+          content_uri: '/uploads/h5p/coronavirus',
           created_at: '2017-12-20 19:17:10',
           updated_at: '2017-12-20 19:17:10'
-        },
-        {
-          id: 'chapter3',
-          name: 'A Chapter3',
-          slug: 'a-chapter3',
-          description: 'An H5P Chapter.',
-          status: 'published',
-          lesson_id: 'lesson1',
-          creator_id: 'user1',
-          content_type: 'h5p',
-          content_uri: '/uploads/h5p/chapter3',
-          created_at: '2017-12-20 19:17:10',
-          updated_at: '2017-12-20 19:17:10'
-        },
-        {
-          id: 'chapter4',
-          name: 'A Chapter4',
-          slug: 'a-chapter4',
-          description: 'An H5P Chapter.',
-          status: 'published',
-          lesson_id: 'lesson2',
-          creator_id: 'user1',
-          content_type: 'h5p',
-          content_uri: '/uploads/h5p/chapter4',
-          created_at: '2017-12-20 19:17:10',
-          updated_at: '2017-12-20 19:17:10'
-        },
-        {
-          'id': 'chapter5',
-          'name': 'Poole',
-          'slug': 'Winnie',
-          'description': 'excepteur',
-          'status': 'published',
-          'lesson_id': 'lesson2',
-          'content_type': 'h5p',
-          'content_uri': '/uploads/h5p/chapter5',
-          'created_at': '2017-12-20 19:17:10',
-          'updated_at': '2017-12-20 19:17:10'
-        },
-        {
-          'id': 'chapter6',
-          'name': 'Carmella',
-          'slug': 'Esther',
-          'description': 'enim',
-          'status': 'published',
-          'lesson_id': 'lesson2',
-          'content_type': 'h5p',
-          'content_uri': '/uploads/h5p/chapter6',
-          'created_at': '2017-12-20 19:17:10',
-          'updated_at': '2017-12-20 19:17:10'
-        },
-        {
-          'id': 'chapter7',
-          'name': 'Randi',
-          'slug': 'Blake',
-          'description': 'dolore',
-          'status': 'published',
-          'lesson_id': 'basics1',
-          'content_type': 'h5p',
-          'content_uri': '/uploads/h5p/chapter7',
-          'created_at': '2017-12-20 19:17:10',
-          'updated_at': '2017-12-20 19:17:10'
-        },
-        {
-          'id': 'chapter8',
-          'name': 'Ball',
-          'slug': 'Bernadette',
-          'description': 'exercitation',
-          'status': 'archived',
-          'lesson_id': 'basics1',
-          'content_type': 'h5p',
-          'content_uri': '/uploads/h5p/chapter8',
-          'created_at': '2017-12-20 19:17:10',
-          'updated_at': '2017-12-20 19:17:10'
-        },
-        {
-          'id': 'chapter9',
-          'name': 'Kelley',
-          'slug': 'Elisa',
-          'description': 'consectetur',
-          'status': 'published',
-          'lesson_id': 'basics1',
-          'content_type': 'h5p',
-          'content_uri': '/uploads/h5p/chapter9',
-          'created_at': '2017-12-20 19:17:10',
-          'updated_at': '2017-12-20 19:17:10'
-        },
-        {
-          'id': 'chapter10',
-          'name': 'Ethel',
-          'slug': 'Tonya',
-          'description': 'cupidatat',
-          'status': 'published',
-          'lesson_id': 'basics2',
-          'content_type': 'h5p',
-          'content_uri': '/uploads/h5p/chapter10',
-          'created_at': '2017-12-20 19:17:10',
-          'updated_at': '2017-12-20 19:17:10'
-        },
-        {
-          'id': 'chapter11',
-          'name': 'Montoya',
-          'slug': 'Myra',
-          'description': 'qui',
-          'status': 'published',
-          'lesson_id': 'basics2',
-          'content_type': 'h5p',
-          'content_uri': '/uploads/h5p/chapter11',
-          'created_at': '2017-12-20 19:17:10',
-          'updated_at': '2017-12-20 19:17:10'
         }
-
       ]);
     });
 };
