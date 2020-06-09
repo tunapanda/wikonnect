62849a6a94b48bebfb613782ac909e4193f08710
Moses Okemwa <okemwamoses@gmail.com>
2020-06-09 10:02:15 +0300

    test migration

--- server/test/_tokens.js
+++ server/test/_tokens.js
@@ -1,5 +1,7 @@
 const jsonwebtoken = require('jsonwebtoken');
 const { secret } = require('../middleware/jwt');
+const knex = require('../db/db');
+
 
 const superAdmin = {
   'user': {
@@ -45,6 +47,14 @@ const headerAdminUser = {
 const brokenToken = {
   'Authorization': 'Bearer ' + jsonwebtoken.sign({ data: adminUser.user }, 'f', { expiresIn: '1d' })
 };
+
+describe('',()=>{
+  before(async () => {
+    await knex.migrate.rollback();
+    await knex.migrate.latest();
+    return knex.seed.run();
+  });
+});
 
 module.exports = {
   headersSuperAdmin1,
