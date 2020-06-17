82cfbef58f2759d2d246377d3d8498279f134b8c
Moses Okemwa <okemwamoses@gmail.com>
2020-06-17 03:41:22 +0300

    login error responses

--- frontend/app/controllers/login.js
+++ frontend/app/controllers/login.js
@@ -1,8 +1,13 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
+import LoginValidations from '../validations/login';
+
 
 
 export default class LoginController extends Controller {
+
+  LoginValidations = LoginValidations;
+
   @action
   login() {
     this.transitionToRoute('/home');
