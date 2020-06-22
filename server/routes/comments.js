e49e550e81563c27844e5e68487ec87c844b46b3
Moses Okemwa <okemwamoses@gmail.com>
2020-06-22 10:18:17 +0300

    comments routes

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -1,13 +1,95 @@
 const Router = require('koa-router');
 
 const Comment = require('../models/comment');
-const { requireAuth, grantAccess} = require('../middleware/permController');
+const { requireAuth, grantAccess } = require('../middleware/permController');
 
 
 const router = new Router({
   prefix: '/comments'
 });
 
+
+/**
+ * @api {get} / GET a comment
+ * @apiName GetChapterComment
+ * @apiGroup ChapterComments
+ * @apiPermission authenticated user
+ *
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 201 OK
+ *     {
+ *      "comment": [{
+ *         "id": String,
+ *         "comment": String,
+ *         "chapterId": String,
+ *         "creatorId": String,
+ *         "createdAt": DateTime,
+ *         "updatedAt": DateTime
+ *        }]
+ *      }
+ *
+ */
+router.get('/', requireAuth, grantAccess('readAny', 'path'), async ctx => {
+  // let stateUserId = ctx.state.user.id == undefined ? ctx.state.user.data.id : ctx.state.user.id;
+
+  let comment;
+  try {
+    comment = await Comment.query();
+  } catch (e) {
+    if (e.statusCode) {
+      ctx.throw(e.statusCode, null, { errors: [e] });
+    } else { ctx.throw(400, null, { errors: [e] }); }
+    throw e;
+  }
+  if (!comment) {
+    ctx.assert(comment, 401, 'Something went wrong');
+  }
+  ctx.status = 201;
+  ctx.body = { comment };
+
+});
+
+
+
+
+/**
+ * @api {get} /:chapterId GET a comment
+ * @apiName GetAChapterComment
+ * @apiGroup ChapterComments
+ * @apiPermission authenticated user
+ *
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 201 OK
+ *     {"comment": [{
+ *          "id": String,
+ *          "chapterId": String,
+ *          "creatorId": String,
+ *          "comment": String,
+ *          "createdAt": DateTime,
+ *          "updatedAt": DateTime
+ *        }]
+ *      }
+ *
+ */
+router.get('/:id', requireAuth, grantAccess('readAny', 'path'), async ctx => {
+  // let stateUserId = ctx.state.user.id == undefined ? ctx.state.user.data.id : ctx.state.user.id;
+
+  let comment;
+  try {
+    comment = await Comment.query().where({ id: ctx.params.id });
+  } catch (e) {
+    if (e.statusCode) {
+      ctx.throw(e.statusCode, null, { errors: [e] });
+    } else { ctx.throw(400, null, { errors: [e] }); }
+    throw e;
+  }
+  if (!comment) {
+    ctx.assert(comment, 401, 'Something went wrong');
+  }
+  ctx.status = 201;
+  ctx.body = { comment };
+
+});
 
 /**
  * @api {post} /:chapterId POST comment
@@ -43,7 +125,38 @@ router.post('/:id', requireAuth, grantAccess('createAny', 'path'), async ctx => 
     throw e;
   }
   if (!comment) {
-    ctx.assert(module, 401, 'Something went wrong');
+    ctx.assert(comment, 401, 'Something went wrong');
+  }
+  ctx.status = 201;
+  ctx.body = { comment };
+
+});
+
+
+/**
+ * @api {put} /:chapterId PUT comment
+ * @apiName PutAChapterComment
+ * @apiGroup ChapterComments
+ * @apiPermission authenticated user
+ *
+ */
+
+router.put('/:id', requireAuth, grantAccess('updateOwn', 'path'), async ctx => {
+  const comment_record = await Comment.query().findById(ctx.params.id);
+  let commentData = ctx.request.body.comment;
+
+  if (!comment_record) {
+    ctx.throw(400, 'No chapter with that ID');
+  }
+
+  let comment;
+  try {
+    comment = await Comment.query().patchAndFetchById(ctx.params.id, commentData);
+  } catch (e) {
+    if (e.statusCode) {
+      ctx.throw(e.statusCode, null, { errors: [e.message] });
+    } else { ctx.throw(400, null, { errors: ['Bad Request'] }); }
+    throw e;
   }
   ctx.status = 201;
   ctx.body = { comment };
