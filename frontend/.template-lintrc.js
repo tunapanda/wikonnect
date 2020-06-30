40d738a4bbef59bbf2d3634d4f361d350067d2e9
IjayAbby <inyangasi4@gmail.com>
2020-06-30 10:40:19 +0300

    fixed lint errors

--- frontend/.template-lintrc.js
+++ frontend/.template-lintrc.js
@@ -3,9 +3,11 @@
 module.exports = {
   extends: 'recommended',
   rules: {
-    'no-obsolete-elements': false,
-    'no-trailing-spaces': false,
-    'attribute-indentation': false,
-    'block-indentation': false
+    'no-obsolete-elements': 1,
+    'no-trailing-spaces': 1
+    // 'no-obsolete-elements': false,
+    // 'no-trailing-spaces': false,
+    // 'attribute-indentation': false,
+    // 'block-indentation': false
   }
 };
