4c754b535465b673e45d2357a4026cc5a4ccbd8b
Moses Okemwa <okemwamoses@gmail.com>
2020-07-07 23:27:40 +0300

    comment seeds

--- server/db/seeds/comment.js
+++ server/db/seeds/comment.js
@@ -4,10 +4,10 @@ exports.seed = function (knex) {
   return knex('comments').del()
     .then(function () {
       return knex('comments').insert([
-        { id: 1, chapterId: 'chapter1', comment: 'chapter1', creatorId: 'user1' },
-        { id: 2, chapterId: 'chapter1', comment: 'chapter1', creatorId: 'user1' },
-        { id: 3, chapterId: 'chapter2', comment: 'chapter2', creatorId: 'user2' },
-        { id: 4, chapterId: 'chapter2', comment: 'chapter2', creatorId: 'user3' }
+        { chapter_id: 'chapter1', comment: 'chapter1', creator_id: 'user1' },
+        { chapter_id: 'chapter1', comment: 'chapter1', creator_id: 'user1' },
+        { chapter_id: 'chapter2', comment: 'chapter2', creator_id: 'user2' },
+        { chapter_id: 'chapter2', comment: 'chapter2', creator_id: 'user3' }
       ]);
     });
 };
