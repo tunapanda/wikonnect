d2007d3c58ead57b02270377010e30336f9f5f0c
Moses Okemwa <okemwamoses@gmail.com>
2020-07-07 21:45:13 +0300

    ember notify for award notification

--- frontend/app/components/h5p.js
+++ frontend/app/components/h5p.js
@@ -1,13 +1,17 @@
 import Component from '@glimmer/component';
 import { H5P, H5PxAPI } from 'h5p-standalone'; // ES6
 import { action } from '@ember/object';
-import { inject } from '@ember/service';
+import { inject, inject as service } from '@ember/service';
 
 
 export default class H5pComponent extends Component {
   @inject me;
+
+  @service notify;
+
   @action
   async renderH5P(el) {
+    this.notify.info('Log in to track progress');
     const h5pLocation = this.args.location;
 
     const options = {
