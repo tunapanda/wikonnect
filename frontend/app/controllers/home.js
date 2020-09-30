295a3a2ae977131f8422cfe53925186eac563465
mosesokemwa <okemwamoses@gmail.com>
2020-09-30 12:03:26 +0300

    lint fixs

--- frontend/app/controllers/home.js
+++ frontend/app/controllers/home.js
@@ -1,5 +1,5 @@
 import Controller from '@ember/controller';
-import { computed, action } from '@ember/object';
+import { computed } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 export default class HomeController extends Controller {
