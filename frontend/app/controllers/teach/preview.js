acf63c236e5b81f27f1e3e096d118e901a85c9a0
Proverbial Ninja <kariuki.marima@gmail.com>
2020-10-07 09:55:01 +0300

    Publish and platform updates

--- frontend/app/controllers/teach/preview.js
+++ frontend/app/controllers/teach/preview.js
@@ -11,40 +11,26 @@ export default class TeachPreviewController extends Controller {
 
 
   @action
-  async publish(chapter_id, a) {
-    console.log("ee");
-    console.log(chapter_id);
-    console.log(a);
+  async publish(chapter_id) {
+    this.store.findRecord('chapter', chapter_id).then(function (chap) {
+      // ...after the record has loaded
 
-    const response = await fetch('../../api/v1/chapters/' + chapter_id, {
-      // const response = await fetch('https://app.wikonnect.org/api/v1/chapters/teach/' + params.chapter_id, {
-      method: 'put',
-      headers: new Headers({
-        'Authorization': 'Bearer ' + this.token,
-      }),
-      body: JSON.stringify({ chapter: { id: chapter_id, status: 'published' } })
-
+      chap.set('status', "published");
+      chap.save();
     });
 
-    console.log(response);
 
   }
 
 
   @action
-  async unpublish(chapter_id, a) {
-    console.log("ee");
-    console.log(chapter_id);
-    console.log(a);
+  async unpublish(chapter_id) {
+    this.store.findRecord('chapter', chapter_id).then(function (chap) {
+      // ...after the record has loaded
 
-    const response = await fetch('../../api/v1/chapters/' + chapter_id, {
-      // const response = await fetch('https://app.wikonnect.org/api/v1/chapters/teach/' + params.chapter_id, {
-      method: 'put',
-      headers: new Headers({
-        'Authorization': 'Bearer ' + this.token,
-      }),
-      body: JSON.stringify({ chapter: { id: chapter_id, status: 'draft', approved: "false" } })
-
+      chap.set('status', "draft");
+      chap.set('approved', false);
+      chap.save();
     });
 
     console.log(response);
--- frontend/app/routes/teach/preview.js
+++ frontend/app/routes/teach/preview.js
@@ -7,31 +7,13 @@ export default class TeachPreviewRoute extends Route {
   @inject
   me
 
-  @inject router;
-
-  @inject session;
-  @tracked token = this.session.data.authenticated.token
 
 
-  async model(params) {
+
+  model(params) {
 
 
-    let p = params;
-    console.log(p);
-    console.log(this.router);
-    console.log("this");
-    const response = await fetch('https://app.wikonnect.org/api/v1/chapters/teach/' + params.id, {
-      // const response = await fetch('https://app.wikonnect.org/api/v1/chapters/teach/' + params.chapter_id, {
-      method: 'get',
-      headers: new Headers({
-        'Authorization': 'Bearer ' + this.token,
-      }),
-
-    });
-
-    const chapters = await response.json();
-    console.log(chapters.chapter[0]);
-    return chapters.chapter[0];
+    return this.store.findRecord('chapter', params.id);
 
 
   }
