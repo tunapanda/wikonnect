a2b66571f34b03ae49d6749c4d4f0e02512f93f9
mosesokemwa <13944912+mosesokemwa@users.noreply.github.com>
2020-10-21 16:25:42 +0300

    ember swagger docs setup, also need auth

---
+++ frontend/app/routes/swagger.js
@@ -0,0 +1,18 @@
+import Route from '@ember/routing/route';
+import { inject } from '@ember/service';
+
+
+export default class SwaggerRoute extends Route {
+
+  @inject
+  me;
+
+  async beforeModel(transition) {
+    if (!this.me.isAuthenticated) {
+      var loginController = this.controllerFor('login');
+      loginController.set('previousTransition', transition);
+      this.transitionTo('login');
+    }
+    return await this.store.findRecord('user', this.me.user.id);
+  }
+}
--- server/routes/users.js
+++ server/routes/users.js
@@ -34,65 +34,6 @@ const router = new Router({
 
 
 /**
-<<<<<<< HEAD
- * @swagger
- * resourcePath: /apiJs
- * description: All about API
- */
-
-/**
- * @swagger
- * path: /users
- * operations:
- *   -  httpMethod: POST
- *      summary: Create a user
- *      notes: Returns a user based on username
- *      responseClass: User
- *      nickname: login
- *      consumes:
- *        - text/html
- *      parameters:
- *        - name: username
- *          description: Your username
- *          paramType: query
- *          required: true
- *          dataType: string
- *        - name: password
- *          description: Your password
- *          paramType: query
- *          required: true
- *          dataType: string
-=======
- * @api {post} /users POST create a new user.
- * @apiName PostAUser
- * @apiGroup Authentication
- *
- * @apiParam (Required Params) {string} user[username] username
- * @apiParam (Required Params) {string} user[email] Unique email
- * @apiParam (Required Params) {string} user[password] validated password
- * @apiParam (Optional Params) {string} user[invitedBy] auto filled on the form
- * @apiParam (Optional Params) {string} user[tags] auto filled on the form
- *
- * @apiPermission none
- *
- * @apiSuccessExample {json} Success-Response:
- *     HTTP/1.1 201 OK
- *     {
- *        "user": {
- *          "id": "string",
- *          "username": "string",
- *          "inviteCode": "invited_by",
- *          "createdAt": "string",
- *          "updatedAt": "string",
- *          "tags": "array",
- *          "metadata": json_array
- *        }
- *     }
->>>>>>> origin/master
- *
- */
-
-/**
 * @api {post} /users POST create a new user.
 * @apiName PostAUser
 * @apiGroup Authentication
@@ -119,20 +60,6 @@ const router = new Router({
 *
 * @apiError {String} errors Bad Request.
 */
-
-
-/**
- * @swagger
- * models:
- *   User:
- *     id: User
- *     properties:
- *       username:
- *         type: String
- *       password:
- *         type: String
- */
-
 
 router.post('/', validateAuthRoutes.validateNewUser, createPasswordHash, async ctx => {
   ctx.request.body.user.username = ctx.request.body.user.username.toLowerCase();
