2be1f644317140ad659480a5d6178346d88334a6
Proverbial Ninja <kariuki.marima@gmail.com>
2020-11-04 08:40:11 +0300

    fix linting

--- frontend/app/controllers/teach/tag.js
+++ frontend/app/controllers/teach/tag.js
@@ -77,30 +77,30 @@ export default class TeachTagController extends Controller {
     console.log(item);
     console.log(cart);
     switch (cart) {
-      case 'competency':
-        this.competency_list.removeObject(item);
-        this.competency_cart.addObject(item);
-        break;
-      case 'topic':
-        this.topic_list.removeObject(item);
-        this.topic_cart.addObject(item);
-        break;
-      case 'level':
-        this.level_list.removeObject(item);
-        this.level_cart.addObject(item);
-        break;
-      case 'platform':
-        this.platform_list.removeObject(item);
-        this.platform_cart.addObject(item);
-        break;
+    case 'competency':
+      this.competency_list.removeObject(item);
+      this.competency_cart.addObject(item);
+      break;
+    case 'topic':
+      this.topic_list.removeObject(item);
+      this.topic_cart.addObject(item);
+      break;
+    case 'level':
+      this.level_list.removeObject(item);
+      this.level_cart.addObject(item);
+      break;
+    case 'platform':
+      this.platform_list.removeObject(item);
+      this.platform_cart.addObject(item);
+      break;
 
-      case 'kicd':
-        this.kicd_list.removeObject(item);
-        this.kicd_cart.addObject(item);
-        break;
+    case 'kicd':
+      this.kicd_list.removeObject(item);
+      this.kicd_cart.addObject(item);
+      break;
 
-      default:
-        break;
+    default:
+      break;
     }
 
 
@@ -112,32 +112,32 @@ export default class TeachTagController extends Controller {
     console.log(cart);
 
     switch (cart) {
-      case 'competency':
-        this.competency_cart.removeObject(item);
-        this.competency_list.addObject(item);
-        break;
-      case 'topic':
-        this.topic_cart.removeObject(item);
-        this.topic_list.addObject(item);
-        break;
-      case 'level':
-        this.level_cart.removeObject(item);
-        this.level_list.addObject(item);
-        break;
-      case 'platform':
-        this.platform_cart.removeObject(item);
-        this.platform_list.addObject(item);
-        break;
-      case 'kicd':
-        this.kicd_cart.removeObject(item);
-        this.kicd_list.addObject(item);
-        break;
-      case 'custom':
-        this.custom_cart.removeObject(item);
-        break;
+    case 'competency':
+      this.competency_cart.removeObject(item);
+      this.competency_list.addObject(item);
+      break;
+    case 'topic':
+      this.topic_cart.removeObject(item);
+      this.topic_list.addObject(item);
+      break;
+    case 'level':
+      this.level_cart.removeObject(item);
+      this.level_list.addObject(item);
+      break;
+    case 'platform':
+      this.platform_cart.removeObject(item);
+      this.platform_list.addObject(item);
+      break;
+    case 'kicd':
+      this.kicd_cart.removeObject(item);
+      this.kicd_list.addObject(item);
+      break;
+    case 'custom':
+      this.custom_cart.removeObject(item);
+      break;
 
-      default:
-        break;
+    default:
+      break;
     }
   }
 
