d37ca7dd0ad2178278d711b9c8a5e1655fe01720
mosesokemwa <13944912+mosesokemwa@users.noreply.github.com>
2020-11-23 16:38:11 +0300

    updated test

--- server/utils/removeTrailingSlashes.js
+++ server/utils/removeTrailingSlashes.js
@@ -1,6 +1,6 @@
 module.exports = function () {
   return async function (ctx, next) {
-    if (process.env.NODE_ENV != 'test' || process.env.NODE_ENV != 'ci') {
+    if (process.env.NODE_ENV != 'test' && process.env.NODE_ENV != 'ci') {
       const redirectUrl = ctx.url
         .replace(/\/?(\?|#|$)/, '/$1');
       if (ctx.url !== redirectUrl && ctx.url !== '/') {
