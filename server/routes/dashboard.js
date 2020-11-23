85f315365e85f28fc1a2aa891374221fcef2218d
mosesokemwa <13944912+mosesokemwa@users.noreply.github.com>
2020-11-23 10:41:08 +0300

    more data exports

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -1,7 +1,5 @@
 const Router = require('koa-router');
 const { requireAuth } = require('../middleware/permController');
-const moment = require('moment');
-const getQuarter = require('date-fns/getQuarter');
 const knex = require('../utils/knexUtil');
 // const compareAsc = require('date-fns/compareAsc');
 // const startOfQuarter = require('date-fns/startOfQuarter');
@@ -11,65 +9,21 @@ const router = new Router({
   prefix: '/dashboard'
 });
 
-/**
- *
- * @param {*} ctx
- * @param {*} next
- */
-async function dateQuery(ctx, next) {
-  const queryQ = ctx.params.quarter.toLowerCase();
-  const queryY = ctx.params.year;
-  const stringQ = {
-    'q1': 1,
-    'q2': 2,
-    'q3': 3,
-    'q4': 4
-  };
-
-  const from = moment().year(queryY).quarter(stringQ[queryQ]);
-  let to = moment(from).endOf('quarter');
-  if (to > moment()) {
-    to = moment();
-  }
-  ctx.state.dateQuery = { from, to };
-  await next();
-}
-
-router.get('/achievements/:quarter/:year', requireAuth, dateQuery, async ctx => {
-  const from = ctx.state.dateQuery.from;
-  const to = ctx.state.dateQuery.to;
-
-  let achievement;
-  try {
-    achievement = await knex('achievements')
-      .select()
-      .where({ target_status: 'completed' })
-      .whereBetween('created_at', [from, to]);
-  } catch (e) {
-    ctx.throw(400, null, { errors: [e.message] });
-    throw e;
-  }
-
-  ctx.status = 200;
-  ctx.body = { achievement: achievement.length, starDate: from, endDate: to };
-});
-
 router.get('/completed', requireAuth, async ctx => {
-  const from = moment().year(2020).quarter(2);
-  const start = getQuarter(new Date(from));
 
-  let completed;
+  let completed, total;
   try {
     /* eslint-disable quotes */
-    completed = await knex.raw("select (extract(year from created_at) || '.Q' || extract(quarter from created_at)) as quarter, count(*) from achievements group by extract(year from created_at), extract(quarter from created_at) having count(target_status) > 1");
+    total = await knex.raw("select count(*) from chapters inner join achievements on chapters.id = target and target_status = 'completed'");
+    completed = await knex.raw("select (extract(year from chapters.created_at) || '.Q' || extract(quarter from chapters.created_at)) as quarter, count(*) from chapters inner join achievements on chapters.id = target and target_status = 'completed' group by extract(year from chapters.created_at), extract(quarter from chapters.created_at)");
   } catch (e) {
     ctx.throw(406, null, { errors: [e.message] });
     throw e;
   }
 
   const data = {
-    completed: completed.rows,
-    quarter: start
+    total: total,
+    completed: completed.rows
   };
 
   ctx.body = { data };
@@ -80,7 +34,7 @@ router.get('/learners', requireAuth, async ctx => {
   let total, quarterly;
   try {
     total = await knex('users').count('*');
-    quarterly = await knex.raw("select count(users.id), (extract(year from achievements.created_at) || '.Q' || extract(quarter from achievements.created_at)) as quarter from users left join achievements on achievements.user_id = users.id group by extract(year from achievements.created_at), extract(quarter from achievements.created_at) having count(achievements.target_status) > 1")
+    quarterly = await knex.raw("select count(users.id), (extract(year from achievements.created_at) || '.Q' || extract(quarter from achievements.created_at)) as quarter from users left join achievements on achievements.user_id = users.id group by extract(year from achievements.created_at), extract(quarter from achievements.created_at) having count(achievements.target_status) > 1");
   } catch (e) {
     ctx.throw(406, null, { errors: [e.message] });
     throw e;
@@ -89,6 +43,25 @@ router.get('/learners', requireAuth, async ctx => {
   const learners = {
     total: total[0],
     quarterly: quarterly.rows,
+  };
+  ctx.body = learners;
+
+});
+
+
+router.get('/content', requireAuth, async ctx => {
+  let total, with_10_chapters;
+  try {
+    total = await knex.raw('select count(distinct creator_id) from chapters');
+    with_10_chapters = await knex.raw("select count(creator_id) from chapters group by creator_id having count(creator_id) > 9");
+  } catch (e) {
+    ctx.throw(406, null, { errors: [e.message] });
+    throw e;
+  }
+
+  const learners = {
+    total: total[0],
+    quarterly: with_10_chapters,
   };
   ctx.body = learners;
 
