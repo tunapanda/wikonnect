f6e99dc2481e194d92d5284b071df7538468b201
Brian Marete <bmarete10@gmail.com>
2021-02-04 21:22:07 +0300

    Add toggle functionality to reactions

--- frontend/app/components/reaction.js
+++ frontend/app/components/reaction.js
@@ -1,4 +1,5 @@
 import Component from "@glimmer/component";
+import { tracked } from "@glimmer/tracking";
 import { inject } from "@ember/service";
 import { action } from "@ember/object";
 
@@ -9,25 +10,109 @@ export default class ReactionComponent extends Component {
   @inject
   store;
 
+  @tracked likes = this.args.likes;
+  @tracked dislikes = this.args.dislikes;
+
   @action
   like(chapterId) {
+
     if (this.me.isAuthenticated) {
-      this.store.createRecord("reaction", {
-        reaction: "like",
-        userId: this.me.user.id,
-        chapterId: chapterId,
-      }).save();
+      const chapter = this.store.peekRecord("chapter", chapterId);
+      const userId = this.me.user.id;
+
+      if (chapter.reaction.authenticated_user === null) {
+        this.store
+          .createRecord("reaction", {
+            reaction: "like",
+            userId: userId,
+            chapterId: chapterId,
+          })
+          .save();
+          this.likes++;
+      } else if (chapter.reaction.authenticated_user === "like") {
+        this.likes--;
+        this.store.findAll("reaction").then((reactions) => {
+          const reaction = reactions.find(
+            (reaction) =>
+              reaction.chapterId === chapterId &&
+              reaction.userId === userId &&
+              reaction.reaction === "like"
+          );
+
+          reaction.destroyRecord();
+        });
+      } else if (chapter.reaction.authenticated_user === "dislike") {
+        this.like++;
+        this.dislike--;
+        this.store.findAll("reaction").then((reactions) => {
+          const reaction = reactions.find(
+            (reaction) =>
+              reaction.chapterId === chapterId &&
+              reaction.userId === userId &&
+              reaction.reaction === "dislike"
+          );
+
+          reaction.destroyRecord();
+          this.store
+            .createRecord("reaction", {
+              reaction: "like",
+              userId: userId,
+              chapterId: chapterId,
+            })
+            .save();
+        });
+      }
     }
   }
 
   @action
   dislike(chapterId) {
     if (this.me.isAuthenticated) {
-      this.store.createRecord("reaction", {
-        reaction: "dislike",
-        userId: this.me.user.id,
-        chapterId: chapterId,
-      }).save();
+      const chapter = this.store.peekRecord("chapter", chapterId);
+      const userId = this.me.user.id;
+
+      if (chapter.reaction.authenticated_user === null) {
+        this.store
+          .createRecord("reaction", {
+            reaction: "dislike",
+            userId: userId,
+            chapterId: chapterId,
+          })
+          .save();
+          this.dislikes++;
+      } else if (chapter.reaction.authenticated_user === "like") {
+        this.store.findAll("reaction").then((reactions) => {
+          const reaction = reactions.find(
+            (reaction) =>
+              reaction.chapterId === chapterId &&
+              reaction.userId === userId &&
+              reaction.reaction === "like"
+          );
+
+          reaction.destroyRecord();
+        });
+        this.store
+          .createRecord("reaction", {
+            reaction: "dislike",
+            userId: userId,
+            chapterId: chapterId,
+          })
+          .save();
+          this.likes--;
+          this.dislikes++;
+      } else if (chapter.reaction.authenticated_user === "dislike") {
+        this.store.findAll("reaction").then((reactions) => {
+          const reaction = reactions.find(
+            (reaction) =>
+              reaction.chapterId === chapterId &&
+              reaction.userId === userId &&
+              reaction.reaction === "dislike"
+          );
+
+          reaction.destroyRecord();
+        });
+        this.dislikes--;
+      }
     }
   }
 }
