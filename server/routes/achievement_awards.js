25d21a38ec039d4c1e428d91b8ab6f9e1639c8e7
Moses Okemwa <13944912+mosesokemwa@users.noreply.github.com>
2021-02-04 12:30:16 +0300

    docs for achievement awards closes #532

--- server/routes/achievement_awards.js
+++ server/routes/achievement_awards.js
@@ -6,6 +6,52 @@ const { requireAuth } = require('../middleware/permController');
 const router = new Router({
   prefix: '/achievementAwards'
 });
+
+
+/**
+* @api {get} /api/v1/achievementAwards POST create a achievements.
+* @apiName GetAchievementAwards
+* @apiGroup AchievementAwards
+*
+* @apiParam {string} achievementAwards[id] optional achievement-award id
+* @apiParam {string} achievementAwards[user_id] optional user id
+* @apiParam {string} achievementAwards[achievementId] optional achievement id
+* @apiParam {string} achievementAwards[metadata] optional
+*
+* @apiPermission none
+* @apiSampleRequest off
+*
+* @apiSuccessExample {json} Success-Response:
+*     HTTP/1.1 200 OK
+*     {
+*        "achievementAwards": [
+*          {
+*            "achievementId": "achievements1",
+*            "userId": "user1",
+*            "createdAt": "2017-12-20T16:17:10.000Z",
+*            "updatedAt": "2017-12-20T16:17:10.000Z",
+*            "name": "longest streak",
+*            "slug": null,
+*            "imageUrl": "/badges/badge1.png",
+*            "metadata": null,
+*            "id": "IrSGD70AAA4"
+*          },
+*          {
+*            "achievementId": "achievements2",
+*            "userId": "user2",
+*            "createdAt": "2017-12-20T16:17:10.000Z",
+*            "updatedAt": "2017-12-20T16:17:10.000Z",
+*            "name": "completed courses",
+*            "slug": null,
+*            "imageUrl": "/badges/badge1.png",
+*            "metadata": null,
+*            "id": "IrSGD8MAAA8"
+*          }
+*        ]
+*      }
+*
+* @apiError {String} errors Bad Request.
+*/
 router.get('/', requireAuth, async ctx => {
   const achievementAwards = await AchievementAward.query().where(ctx.query);
   ctx.assert(achievementAwards, 404, 'No matching record found');
@@ -13,6 +59,32 @@ router.get('/', requireAuth, async ctx => {
   ctx.body = { achievementAwards };
 });
 
+/**
+* @api {get} /api/v1/achievements/:id GET an achievement.
+* @apiName GetAnAchievementAward
+* @apiGroup AchievementAwards
+*
+* @apiPermission none
+*
+* @apiSuccessExample {json} Success-Response:
+*
+* @apiSuccessExample {json} Success-Response:
+*     HTTP/1.1 200 OK
+*     {
+*        "achievementAwards": {
+*            "achievementId": "achievements1",
+*            "userId": "user1",
+*            "createdAt": "2017-12-20T16:17:10.000Z",
+*            "updatedAt": "2017-12-20T16:17:10.000Z",
+*            "name": "longest streak",
+*            "slug": null,
+*            "imageUrl": "/badges/badge1.png",
+*            "metadata": null,
+*            "id": "IrSGD70AAA4"
+*          }
+*      }
+* @apiError {String} errors Bad Request.
+*/
 router.get('/:id', requireAuth, async ctx => {
   let achievementAwards = await AchievementAward.query().findById(ctx.params.id);
   const date = new Date(achievementAwards.createdAt);
