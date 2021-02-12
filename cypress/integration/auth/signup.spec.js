f293a0db09e9c3fe1dc96f236d77c6ec4cafc906
mimidots <murage@labskenya.com>
2021-02-12 11:38:29 +0300

    Basic password form user signup tests

---
+++ cypress/integration/auth/signup.spec.js
@@ -0,0 +1,100 @@
+import user from '../../fixtures/user.json';
+
+describe('Sign up', () => {
+
+    beforeEach(() => {
+        cy.visit('/signup')
+    })
+
+    it('should not register user without data', () => {
+
+        cy.get('#email input[type="text"]')
+            .type('invalid@email')
+            .blur()
+            .get('#email .invalid-feedback')
+            .should('be.visible');
+
+        cy.get('.form button[type="submit"]')
+            .click();
+
+        cy.get('#username .invalid-feedback')
+            .should('be.visible')
+            .get('#email .invalid-feedback')
+            .should('be.visible')
+            .get('#password .invalid-feedback')
+            .should('be.visible')
+            .url()
+            .should('include', '/signup');
+    })
+
+    it('should register new  user', () => {
+
+        const username = 'dummyaccount';
+        const password = 'dummypassword';
+        const email = 'winnie@dummyemail.com';
+
+        cy.get('.signup-form-fields #username input[type="text"]')
+            .type(username)
+            .should('have.value', username);
+
+        cy.get('.signup-form-fields #email input[type="text"]')
+            .type(email)
+            .should('have.value', email);
+
+        cy.get('.signup-form-fields #password input[type="password"]')
+            .type(password)
+            .should('have.value', password);
+
+        cy.get('.signup-form-fields #confirm-password input[type="password"]')
+            .type(password)
+            .should('have.value', password);
+
+
+        cy.get('.form button[type="submit"]')
+            .click()
+            .url()
+            .should('include', '/upload')
+    })
+
+    it('should not register existing user', () => {
+        cy.fixture('user')
+            .then(({username, password, email}) => {
+
+                cy.get('.signup-form-fields #username input[type="text"]')
+                    .type(username)
+                    .should('have.value', username);
+
+                cy.get('.signup-form-fields #email input[type="text"]')
+                    .type(email)
+                    .should('have.value', email);
+
+                cy.get('.signup-form-fields #password input[type="password"]')
+                    .type(password)
+                    .should('have.value', password);
+
+                cy.get('.signup-form-fields #confirm-password input[type="password"]')
+                    .type(password)
+                    .should('have.value', password);
+
+
+                cy.get('.form button[type="submit"]')
+                    .click();
+
+                cy.get('.alert.alert-danger')
+                    .should('be.visible')
+                    .should('contain.text', 'username already exists')
+            });
+    })
+
+    it('should be redirected if already authenticated', () => {
+        cy.login();
+        cy.visit('/signup')
+            .url()
+            .should('not.include', 'signup')
+    })
+
+    it('links to login page', () => {
+        cy.get('a[href="/login"]')
+            .should('be.visible');
+    });
+})
