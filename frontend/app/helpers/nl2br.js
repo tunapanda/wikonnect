914dfa6744ebe828a8bf5d630571a37f913d91fd
murage <murage@labskenya.com>
2021-02-15 17:45:57 +0300

    [WIK-46] Posting a lesson comment should immediately update and give user feedback. (#562)
    * update the comment section automatically after posting and provide feedback on the process
    
    * - Add helper function to allow line breaks preservation during string interpolation.
    
    Signed-off-by: mimidots <murage@labskenya.com>
    
    * Preserve posted comments line breaks
    
    Signed-off-by: mimidots <murage@labskenya.com>
    
    * add chapter comment component tests
    
    * Fix comment message deleted on posting error
    
    Signed-off-by: mimidots <murage@labskenya.com>
    
    * Use Ember development builds to fix CircleCi integration test fails
    
    Co-authored-by: mimidots <25152892+mimidotsuser@users.noreply.github.com>
    Co-authored-by: Kiki <kariuki.marima@gmail.com>

---
+++ frontend/app/helpers/nl2br.js
@@ -0,0 +1,10 @@
+import {helper} from '@ember/component/helper';
+import {htmlSafe} from '@ember/string';
+import Ember from 'ember';
+
+
+export default helper(function nl2br(str) {
+  const value = Ember.Handlebars.Utils.escapeExpression(str);
+
+  return htmlSafe(value.replace(/\n/g, '<br/>'));
+});
