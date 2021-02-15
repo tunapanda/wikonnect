28d37319e011c21a39aa7a0a2da47e6d6e0ca0b4
Moses Okemwa <13944912+mosesokemwa@users.noreply.github.com>
2021-02-15 17:04:38 +0300

    edit work as of this point
    Signed-off-by: Moses Okemwa <13944912+mosesokemwa@users.noreply.github.com>

--- frontend/app/routes/teach/index.js
+++ frontend/app/routes/teach/index.js
@@ -6,6 +6,6 @@ export default class TeachIndexRoute extends Route {
 
   @service me
   async model() {
-    return this.store.query('chapter', { 'creatorId': this.me.user.id });
+    return this.store.query('chapter', { 'creatorId': this.me.user.id, status: 'draft'});
   }
 }
