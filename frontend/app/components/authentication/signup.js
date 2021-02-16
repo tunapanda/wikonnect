11f4f670007836be75c00dd378cb840a14e8e7ca
Moses Okemwa <13944912+mosesokemwa@users.noreply.github.com>
2021-02-16 09:31:31 +0300

    social auth using google and pre-setup for facebook [WIK-24] (#571)
    * google auth login works well
    
    Signed-off-by: Moses Okemwa <13944912+mosesokemwa@users.noreply.github.com>
    
    * lint fixes
    
    Signed-off-by: Moses Okemwa <13944912+mosesokemwa@users.noreply.github.com>
    
    * cleaner start scripts
    
    Signed-off-by: Moses Okemwa <13944912+mosesokemwa@users.noreply.github.com>
    
    * hbs lint fixes
    
    Signed-off-by: Moses Okemwa <13944912+mosesokemwa@users.noreply.github.com>
    
    * testing lint-staged
    
    Signed-off-by: Moses Okemwa <13944912+mosesokemwa@users.noreply.github.com>
    
    * pre-commit linter
    
    Signed-off-by: Moses Okemwa <13944912+mosesokemwa@users.noreply.github.com>
    
    * pre-commit linter
    
    Signed-off-by: Moses Okemwa <13944912+mosesokemwa@users.noreply.github.com>
    
    * linter fixes
    
    Signed-off-by: Moses Okemwa <13944912+mosesokemwa@users.noreply.github.com>
    
    Co-authored-by: Kiki <kariuki.marima@gmail.com>

--- frontend/app/components/authentication/signup.js
+++ frontend/app/components/authentication/signup.js
@@ -25,20 +25,17 @@ export default class AuthenticationSignupComponent extends Component {
   torii
 
   @action
-  sessionRequiresAuthentication() {
-    this.notify.info('Signing up...', { closeAfter: 5000 });
-
-    const me = this.me;
-    this.get('torii')
-      .open('google-oauth2-bearer')
-      .then(function (googleAuth) {
-        const googleToken = googleAuth.authorizationToken.access_token;
-
-        me.registerWithGoogle({ googleToken: googleToken, provider: 'google' })
-          .then((user) => me.authenticate(user.get('username'), googleToken));
-      }, function (error) {
-        console.error('Google auth failed: ', error.message);
-      });
+  authenticateWithGoogleImplicitGrant() {
+    let clientId = this.config.get('google').apiKey;
+    let redirectURI = `${window.location.origin}/callback`;
+    let responseType = 'token';
+    let scope = 'profile email';
+    window.location.replace('https://accounts.google.com/o/oauth2/v2/auth?'
+      + `client_id=${clientId}`
+      + `&redirect_uri=${redirectURI}`
+      + `&response_type=${responseType}`
+      + `&scope=${scope}`
+    );
   }
 
   @action
@@ -69,5 +66,4 @@ export default class AuthenticationSignupComponent extends Component {
       }
     });
   }
-
 }
