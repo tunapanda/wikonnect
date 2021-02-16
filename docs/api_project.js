ddccb53834f8bbe7c9e8fa8220e6c918b66e9b40
Moses Okemwa <13944912+mosesokemwa@users.noreply.github.com>
2021-02-16 08:42:08 +0300

    oembed post double query params bugfix (#574)
    * oembed bug fix on double query params
    
    Signed-off-by: Moses Okemwa <13944912+mosesokemwa@users.noreply.github.com>
    
    * fixed docs sample request error
    
    Signed-off-by: Moses Okemwa <13944912+mosesokemwa@users.noreply.github.com>
    
    Co-authored-by: Kiki <kariuki.marima@gmail.com>

--- docs/api_project.js
+++ docs/api_project.js
@@ -26,7 +26,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-02-05T07:36:39.866Z",
+    "time": "2021-02-10T12:59:28.026Z",
     "url": "https://apidocjs.com",
     "version": "0.26.0"
   }
