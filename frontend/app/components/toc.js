1e8197003ce3a990ddc94ed1ea44028828cae79f
Brian Marete <bmarete10@gmail.com>
2021-02-16 07:59:37 +0300

    fix linting errors

--- frontend/app/components/toc.js
+++ frontend/app/components/toc.js
@@ -1,11 +1,11 @@
-import Component from "@glimmer/component";
-import { action } from "@ember/object";
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
 
 export default class TocComponent extends Component {
   @action
   highlight(element) {
     const listItems = Array.from(element.children);
     const active = listItems.slice(0, this.args.page);
-    active.forEach((li) => li.classList.add("active"));
+    active.forEach((li) => li.classList.add('active'));
   }
 }
