b32e8c858c0d07fbb9f525af75d268fd645430dd
Brian Marete <bmarete10@gmail.com>
2021-02-24 11:27:38 +0300

    circle ci fix

--- cypress/integration/teach/create.spec.js
+++ cypress/integration/teach/create.spec.js
@@ -26,7 +26,7 @@ describe('User can create chapter', () => {
     // Thumbnail Upload
     cy.intercept(/api\/v1\/chapters\/.*\/chapter-image/).as("imgUpload");
     cy.get('input[type="file"]').attachFile('test.jpg');
-    cy.wait("@imgUpload");
+    cy.wait(6000);
 
     // Tag
     cy.get('.p-4 input.form-control').type(tag);
