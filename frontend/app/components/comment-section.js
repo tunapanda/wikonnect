15c031a967f3d2f96645df22f260a7f410de5490
Moses Okemwa <13944912+mosesokemwa@users.noreply.github.com>
2021-02-24 18:20:15 +0300

    fixed breaking changes for access token and profile uri (#602)
    * fixed breaking changes for access token and profile uri
    
    * tweaks in tested code
    
    Signed-off-by: Moses Okemwa <13944912+mosesokemwa@users.noreply.github.com>
    
    * reaction tests
    
    Signed-off-by: Moses Okemwa <13944912+mosesokemwa@users.noreply.github.com>
    
    * fix #user id undefined when creating comment model record
    
    Co-authored-by: mimidots <murage@labskenya.com>

--- frontend/app/components/comment-section.js
+++ frontend/app/components/comment-section.js
@@ -24,8 +24,8 @@ export default class CommentSectionComponent extends Component {
 
   get commentModel() {
     return this.store.createRecord('comment', {
-      user: this.me.user.id,
-      chapter: this.me.user.id,
+      user: this.me.user?.id,
+      chapter: this.args.selectedChapter,
       comment: this.comment
     });
   }
