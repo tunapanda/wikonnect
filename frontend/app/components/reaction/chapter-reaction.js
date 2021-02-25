d8c35625320ea2ffb1c23e76748e137c4117960c
Brian Marete <bmarete10@gmail.com>
2021-02-25 17:20:21 +0300

    Reactions hotfix (#604)
    * Update reaction component
    
    * Update Reaction tests
    
    * Linting fixes

--- frontend/app/components/reaction/chapter-reaction.js
+++ frontend/app/components/reaction/chapter-reaction.js
@@ -4,30 +4,38 @@ import {action} from '@ember/object';
 import {tracked} from '@glimmer/tracking';
 
 export default class ReactionChapterReactionComponent extends Component {
-
   @service me;
   @service store;
 
   @tracked
-  likes = Array.isArray(this.args.chapter.reaction) ? 0 : this.args.chapter.reaction.likes || 0;
+  likes = +(this.args.chapter?.reaction[0]
+    ? this.args.chapter.reaction[0]?.likes
+    : 0);
+
   @tracked
-  dislikes = Array.isArray(this.args.chapter.reaction) ? 0 : this.args.chapter.reaction?.dislikes || 0;
+  dislikes = +(this.args.chapter?.reaction[0]
+    ? this.args.chapter.reaction[0]?.dislikes
+    : 0);
 
   get chapter() {
     return this.args.chapter;
   }
 
+  @tracked
+  hasLiked =
+    !this.me.isAuthenticated ||
+    !this.args.chapter?.reaction[0] ||
+    !this.chapter?.authenticatedUser
+      ? false
+      : this.chapter.authenticatedUser === 'like';
 
   @tracked
-  hasLiked = (!this.me.isAuthenticated ||Array.isArray(this.args.chapter.reaction)||
-    !this.chapter?.reaction?.authenticated_user) ? false :
-    this.chapter.reaction.authenticated_user === 'like';
-
-  @tracked
-  hasDisliked = (!this.me.isAuthenticated ||Array.isArray(this.args.chapter.reaction)||
-    !this.chapter?.reaction?.authenticated_user) ? false :
-    this.chapter.reaction.authenticated_user === 'dislike';
-
+  hasDisliked =
+    !this.me.isAuthenticated ||
+    !this.args.chapter?.reaction[0] ||
+    !this.chapter?.authenticatedUser
+      ? false
+      : this.chapter.authenticatedUser === 'dislike';
 
   @action
   async userChapterReaction(chapter, liked = true) {
@@ -41,24 +49,28 @@ export default class ReactionChapterReactionComponent extends Component {
         .then(() => {
           this.updateUserReaction(liked);
           this.updateReactionsCount(chapter, liked ? 1 : 0, liked ? 0 : 1);
-        }).catch(() => {
-        });
+        })
+        .catch(() => {});
     }
 
-    let previousReaction = (await this.store
-      .query('reaction', {'chapterId': this.chapter.id, 'user_id': this.me.user.id}))
-      .find((reactions) => reactions.chapter.get('id') === this.chapter.id);
+    let previousReaction = (
+      await this.store.query('reaction', {
+        chapterId: this.chapter.id,
+        user_id: this.me.user.id,
+      })
+    ).find((reactions) => reactions.chapter.get('id') === this.chapter.id);
 
     //if is the same, they have retracted it, delete/undo the reaction
-    if ((this.hasLiked && liked === true) ||
-      (this.hasDisliked && liked === false)) {
+    if (
+      (this.hasLiked && liked === true) ||
+      (this.hasDisliked && liked === false)
+    ) {
       return this.deleteUserChapterReaction(previousReaction)
         .then(() => {
           this.updateUserReaction();
           this.updateReactionsCount(chapter, liked ? -1 : 0, liked ? 0 : -1);
-        }).catch(() => {
-
-        });
+        })
+        .catch(() => {});
     }
 
     // if they switch, update the reaction
@@ -66,17 +78,15 @@ export default class ReactionChapterReactionComponent extends Component {
       .then(() => {
         this.updateUserReaction(liked);
         this.updateReactionsCount(chapter, liked ? 1 : -1, liked ? -1 : 1);
-      }).catch(() => {
-      });
-
+      })
+      .catch(() => {});
   }
-
 
   async createUserChapterReaction(chapter, liked) {
     const model = this.store.createRecord('reaction', {
       reaction: liked ? 'like' : 'dislike',
       chapter: chapter,
-      user: this.me.user
+      user: this.me.user,
     });
     return await model.save();
   }
@@ -99,5 +109,4 @@ export default class ReactionChapterReactionComponent extends Component {
     this.hasDisliked = liked == null ? false : liked === false;
     this.hasLiked = liked == null ? false : liked === true;
   }
-
 }
