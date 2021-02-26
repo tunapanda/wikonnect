679628ed86aa5ce4c510c255b5ead78fcab8a670
Brian Marete <bmarete10@gmail.com>
2021-02-26 15:59:42 +0300

    Update Chapter model

--- frontend/app/models/chapter.js
+++ frontend/app/models/chapter.js
@@ -15,7 +15,7 @@ export default class ChapterModel extends Model {
   @attr imageUrl;
   @attr createdAt;
   @attr reaction;
-  @attr authenticatedUser;
+  @attr authenticated_user;
   @hasMany('comment') comments;
 
   @belongsTo('user') creator;
