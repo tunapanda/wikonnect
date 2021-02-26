0b23f9c76b391a2013cba86486c76c27f5910bb7
mimidots <murage@labskenya.com>
2021-02-26 17:55:54 +0300

    remove unused route and respective chapters
    Signed-off-by: mimidots <murage@labskenya.com>

--- frontend/app/controllers/cms/index.js
+++
@@ -1,9 +0,0 @@
-import Controller from '@ember/controller';
-import { inject } from '@ember/service';
-
-
-export default class CmsIndexController extends Controller {
-  @inject
-  me
-
-}
--- frontend/app/router.js
+++ frontend/app/router.js
@@ -11,80 +11,9 @@ Router.map(function () {
   this.route('login');
   this.route('home');
 
-  this.route('course', function () {
-    this.route('index', {
-      path: '/:course_slug'
-    });
-
-    this.route('module', function () {
-      this.route('index', {
-        path: '/:module_slug'
-      });
-
-      this.route('lesson', function () {
-        this.route('index', {
-          path: '/:lesson_slug'
-        });
-
-        this.route('chapter', function () {
-          this.route('index', {
-            path: '/:chapter_slug'
-          });
-        });
-      });
-
-      this.route('edit', {
-        path: '/edit/:module_slug'
-      });
-    });
-    this.route('create');
-
-    this.route('edit', {
-      path: '/edit/:course_slug'
-    });
-  });
-
   this.route('profile');
   this.route('search', { path: '/search/:id' });
-  this.route('about');
 
-  this.route('cms', function () {
-    this.route('lesson', function () {
-      this.route('create');
-
-      this.route('edit', {
-        path: '/:lesson_slug'
-      });
-    });
-
-    this.route('course', function () {
-      this.route('create');
-      this.route('edit', {
-        path: '/:course_slug'
-      });
-    });
-
-    this.route('module', function () {
-      this.route('edit', {
-        path: '/:module_slug'
-      });
-      this.route('create');
-    });
-  });
-
-  this.route('module', function () {
-    this.route('create');
-    this.route('index', {
-      path: '/:module_slug'
-    });
-  });
-  this.route('courses');
-  this.route('cms-desktop');
-  this.route('lesson', function () {
-    this.route('index', {
-      path: '/:lesson_slug'
-    });
-  });
   this.route('chapter', function () {
     this.route('index', {
       path: '/:chapter_slug'
@@ -108,8 +37,7 @@ Router.map(function () {
     this.route('edit', { path: '/edit/:chapter_id' });
 
   });
-  this.route('topics');
-  this.route('tags');
+  this.route('swagger');
   this.route('tag', { path: '/tag/:id' });
   this.route('embed', { path: '/embed/:chapter_id' });
   this.route('callback');
