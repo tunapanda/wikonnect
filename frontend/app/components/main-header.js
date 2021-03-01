f53dd8403aaa3c8abeaf96d776a40817af948675
mimidots <murage@labskenya.com>
2021-03-01 15:39:57 +0300

    fix #Octane component imports from the @glimmer package namespace
    Signed-off-by: mimidots <murage@labskenya.com>

--- frontend/app/components/main-header.js
+++ frontend/app/components/main-header.js
@@ -1,4 +1,4 @@
-import Component from '@ember/component';
+import Component from '@glimmer/component';
 import {inject as service} from '@ember/service';
 import {action} from '@ember/object';
 import {tracked} from '@glimmer/tracking';
