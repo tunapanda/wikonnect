8fd8f2b4810c564c44849a44caba24587cc2f8af
mimidots <murage@labskenya.com>
2021-03-02 00:46:08 +0300

    fix #infinite loop on manage model query after removing computed property
    Signed-off-by: mimidots <murage@labskenya.com>

--- frontend/app/routes/manage.js
+++ frontend/app/routes/manage.js
@@ -1,4 +1,8 @@
 import Route from '@ember/routing/route';
 
 export default class ManageRoute extends Route {
+
+  model() {
+    return this.store.query('chapter', {'status': 'published'});
+  }
 }
