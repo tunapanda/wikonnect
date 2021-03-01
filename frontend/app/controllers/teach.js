260373dd2901931684da7a28d588c695600215a7
mimidots <murage@labskenya.com>
2021-03-02 02:18:40 +0300

    remove commented imports
    Signed-off-by: mimidots <murage@labskenya.com>

--- frontend/app/controllers/teach.js
+++ frontend/app/controllers/teach.js
@@ -1,8 +1,4 @@
 import Controller from '@ember/controller';
-// import { inject } from '@ember/service';
-//import { computed, action } from '@ember/object';
-// import { tracked } from '@glimmer/tracking';
-
 
 export default class TeachController extends Controller {
 
