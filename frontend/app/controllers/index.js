ffe877f5c2088bd3e622ffbad4b7076baf6f0613
mimidots <murage@labskenya.com>
2021-03-01 22:09:47 +0300

    refactored home controller
    Signed-off-by: mimidots <murage@labskenya.com>

--- frontend/app/controllers/index.js
+++ frontend/app/controllers/index.js
@@ -1,20 +1,14 @@
 import Controller from '@ember/controller';
-import { inject } from '@ember/service';
-import { computed } from '@ember/object';
-import { tracked } from '@glimmer/tracking';
+import {inject as service} from '@ember/service';
+import {tracked} from '@glimmer/tracking';
 
 export default class IndexController extends Controller {
 
-
-  @inject
-  me;
-
-  @inject
-  session;
+  @service me;
+  @service session;
 
   @tracked loggedIn;
 
-  @computed()
   get myProfile() {
     this.loggedIn = this.me.user;
     return this.me.a;
