4c5aeea9102996145ddefa2dbaabe4dede706a53
mimidots <murage@labskenya.com>
2021-03-02 15:12:40 +0300

    v3.19.0...v3.20.2
    Signed-off-by: mimidots <murage@labskenya.com>

--- frontend/tests/test-helper.js
+++ frontend/tests/test-helper.js
@@ -1,5 +1,5 @@
-import Application from '../app';
-import config from '../config/environment';
+import Application from 'wikonnect/app';
+import config from 'wikonnect/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
 
