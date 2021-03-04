a7b7810e0dcab31973781d3e82e17719d518d36d
Brian Marete <bmarete10@gmail.com>
2021-03-04 16:04:57 +0300

    Update chapters serializer to remove views from data sent to server

--- frontend/app/serializers/chapter.js
+++ frontend/app/serializers/chapter.js
@@ -3,8 +3,10 @@ import ApplicationSerializer from './application';
 export default class ChapterSerializer extends ApplicationSerializer {
   serialize() {
     let json = super.serialize(...arguments);
-    // Sending authenticatedUser to backend causes an error so we avoid sending it
+    // Sending authenticatedUser & views to backend causes an error so we avoid sending it
     delete json.authenticatedUser;
+    delete json.views;
+
     return json;
   }
 }
