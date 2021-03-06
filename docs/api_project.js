e281226a7e6644414b92c686bc3765a03ba4bd04
mosesokemwa <13944912+mosesokemwa@users.noreply.github.com>
2021-03-07 00:46:19 +0300

    fixed docs error

--- docs/api_project.js
+++ docs/api_project.js
@@ -26,7 +26,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-03-06T21:33:49.044Z",
+    "time": "2021-03-06T21:46:08.075Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
