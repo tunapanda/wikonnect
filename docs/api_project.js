e2b807194c2f64779c3ed1756658eae3bf4bc485
mosesokemwa <13944912+mosesokemwa@users.noreply.github.com>
2021-03-07 00:28:59 +0300

    docs degeration
    Signed-off-by: mosesokemwa <13944912+mosesokemwa@users.noreply.github.com>

--- docs/api_project.js
+++ docs/api_project.js
@@ -26,7 +26,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-03-04T16:18:43.544Z",
+    "time": "2021-03-06T21:28:33.997Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
