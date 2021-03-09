a2257940262ffa3b21943d3acb30ce8a4718c528
mimidots <murage@labskenya.com>
2021-03-09 14:59:10 +0300

    refactored teach layout user authentication check
    Signed-off-by: mimidots <murage@labskenya.com>

--- frontend/app/routes/teach/create.js
+++ frontend/app/routes/teach/create.js
@@ -4,17 +4,6 @@ import { inject as service } from '@ember/service';
 export default class TeachCreateRoute extends Route {
   @service me;
 
-  beforeModel(transition) {
-    if (!this.me.isAuthenticated) {
-      if (!this.me.isAuthenticated) {
-        // eslint-disable-next-line ember/no-controller-access-in-routes
-        let loginController = this.controllerFor('login');
-        loginController.set('previousTransition', transition);
-      }
-      this.transitionTo('login');
-    }
-  }
-
   model() {
     return this.store.createRecord('chapter', {
       creator: this.me.get('user'),
