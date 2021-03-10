b8d30ed9c88cad6a039afacd87fe9c8232128ed4
mimidots <murage@labskenya.com>
2021-03-10 11:05:41 +0300

    lint fix

--- frontend/app/routes/home.js
+++ frontend/app/routes/home.js
@@ -1,6 +1,5 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
-import { action } from '@ember/object';
 
 export default class HomeRoute extends Route {
   @service me;
