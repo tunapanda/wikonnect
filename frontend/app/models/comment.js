962694d74df1bb77186e4061e35753908be3a826
Brian Marete <bmarete10@gmail.com>
2021-03-11 10:57:57 +0300

    Update single chapter creator and description sections

--- frontend/app/models/comment.js
+++ frontend/app/models/comment.js
@@ -1,7 +1,9 @@
-import Model, { belongsTo, attr } from '@ember-data/model';
+import Model, { belongsTo, attr, hasMany } from '@ember-data/model';
 
 export default class CommentModel extends Model {
   @attr comment;
+  // @attr children;
+  @hasMany('comment', {inverse: null}) replies;
   @belongsTo('chapter') chapter;
   @belongsTo('user') creator;
 }
