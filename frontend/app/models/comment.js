a1547a938784738191920e08ef7bbae350f07e17
Brian Marete <bmarete10@gmail.com>
2021-03-12 15:50:44 +0300

    Update comment model to have replies

--- frontend/app/models/comment.js
+++ frontend/app/models/comment.js
@@ -2,8 +2,8 @@ import Model, { belongsTo, attr, hasMany } from '@ember-data/model';
 
 export default class CommentModel extends Model {
   @attr comment;
-  // @attr children;
-  @hasMany('comment', {inverse: null}) replies;
+  @attr parent;
+  @hasMany('comment', { inverse: null }) replies;
   @belongsTo('chapter') chapter;
   @belongsTo('user') creator;
 }
