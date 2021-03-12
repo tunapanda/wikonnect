fe5841ce292847dd764881c6ad75e878422842b4
mimidots <murage@labskenya.com>
2021-03-12 11:45:45 +0300

    Fixed source folder name & updated to provide actions feedback

--- server/scripts/h5p-content-migration.sh
+++ server/scripts/h5p-content-migration.sh
@@ -1,36 +1,36 @@
-sourceH5PFolder=../public/uplaods/h5p/
-destinationH5PFolder=../public/h5p/
-
-
-
+sourceH5PFolder=public/uploads/h5p
+destinationH5PFolder=public/h5p
 
 #enable extglob extension to allows pattern feature matching
 shopt -s extglob
-
 
 librariesFolder=$destinationH5PFolder/libraries
 
 #create libraries folder if it does not exist
 mkdir -p $librariesFolder
 
-
 #Select all content in source
-oldContent=$(ls $sourceH5PFolder )
+oldContent=$(ls $sourceH5PFolder)
+
+count=0
+
+for folder in $oldContent; do
+  echo -e "Copying content in $folder folder .."
+  #create content folder if does not exist
+  mkdir -p $destinationH5PFolder/content/$folder
+
+  #copy contents  without overwriting
+  cp -p -R $sourceH5PFolder/$folder/content/* $destinationH5PFolder/content/$folder
+
+  #copy H5P main definition file  without overwriting
+  cp -p  $sourceH5PFolder/$folder/h5p.json $destinationH5PFolder/content/$folder
+
+  #copy libraries without overwriting
+  cp -p -R $sourceH5PFolder/$folder/!(content)/ $librariesFolder
 
 
-for folder in $oldContent
-    do
-
-        #create content folder if does not exist
-        mkdir -p $destinationH5PFolder/content/$folder
-
-        #copy contents
-        cp -v -p -R -i $sourceH5PFolder/$folder/content/* $destinationH5PFolder/content/$folder
-
-        #copy H5P main definition file
-        cp -v -p  $sourceH5PFolder/$folder/h5p.json $destinationH5PFolder/content/$folder
-
-        #copy libraries without overwriting
-        cp -v -p  -n  -R $sourceH5PFolder/$folder/!(content)/ $librariesFolder
-
+  echo -e "Content in $folder migrated into new directory structure \n"
+  ((count = count + 1))
 done
+
+echo -e "Total migrated content : $count \n. All content migrated successfully"
