ca18d626d1592a5cd3306a5cb4d43ca6c9bd8cfe
mimidots <murage@labskenya.com>
2021-03-15 14:47:01 +0300

    chapter pagination tests

--- cypress/integration/home/index.spec.js
+++ cypress/integration/home/index.spec.js
@@ -1,17 +1,41 @@
-describe('Landing Page',()=>{
+describe('Homepage', () => {
 
-    beforeEach(()=>{
+    beforeEach(() => {
         cy.visit('/');
     });
 
-    it('links to login view',()=>{
+    it('Should link to login page', () => {
         cy.get('a[href="/login"]')
             .should('be.visible');
     });
 
-    it('links to registration view',()=>{
+    it('Should link to registration page', () => {
         cy.get('a[href="/signup"]')
             .should('be.visible')
     });
 
+    it('Should load chapters', () => {
+        cy.get('#chapters-container .chapter-card .chapter-title')
+            .should('be.visible')
+            .its('length')
+            .should('be.gt', 1)
+
+    });
+
+    it('Should load extra chapters scroll', ()=> {
+        cy.get('#chapters-container .chapter-card')
+            .its('length')
+            .then((chapterCount)=>{
+
+
+                cy.get('.infinity-loader')
+                    .scrollIntoView({ duration: 500 });
+               
+               cy.wait(900)
+                .get('#chapters-container .chapter-card')
+                    .its('length')
+                    .should('be.gt', chapterCount)
+            })
+
+    });
 })
