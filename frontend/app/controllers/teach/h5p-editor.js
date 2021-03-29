4dc46a03e73863ee19c5469f9c567f3b18031988
murage <murage@labskenya.com>
2021-03-29 23:34:42 +0300

    rollback to old H5P upload form (#666)
    Signed-off-by: mimidotsuser <murage@labskenya.com>

---
+++ frontend/app/controllers/teach/h5p-editor.js
@@ -0,0 +1,35 @@
+import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+
+export default class TeachH5pEditorController extends Controller {
+  @service me;
+  @service notify;
+
+  @tracked editor;
+
+  @action
+  onEditorInitialized(elem) {
+    this.editor = elem;
+  }
+
+  @action
+  async saveH5PContent() {
+    if (!this.editor) {
+      this.notify.info('You need to create content first to proceed');
+      return;
+    }
+    try {
+      const res = await this.editor.save();
+      this.model.contentId = res.id;
+      await this.model.save();
+      this.transitionToRoute('teach.thumbnail-upload', this.model.id);
+    } catch (e) {
+      console.log(e);
+      this.notify.alert(
+        'Unexpected error encountered and we could not save the content'
+      );
+    }
+  }
+}
--- frontend/app/controllers/teach/h5p-upload.js
+++ frontend/app/controllers/teach/h5p-upload.js
@@ -1,34 +1,36 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
+import Uploader from '../../utils/uploader';
 import { tracked } from '@glimmer/tracking';
 
 export default class TeachH5pUploadController extends Controller {
   @service me;
   @service notify;
 
-  @tracked editor;
+  @tracked uploader;
+  @tracked complete = false;
 
   @action
-  onEditorInitialized(elem) {
-    this.editor = elem;
-  }
-
-  @action
-  async saveH5PContent() {
-    if (!this.editor) {
-      this.notify.info('You need to create content first to proceed');
-      return;
-    }
+  async uploadH5p(files) {
     try {
-      const res = await this.editor.save();
-      this.model.contentId = res.id;
-      await this.model.save();
-      this.transitionToRoute('teach.thumbnail-upload', this.model.id);
+      let id = this.model.id;
+      this.uploader = Uploader.create({
+        file: files[0],
+        filename: files[0].name,
+      });
+
+      const host = '/' + this.store.adapterFor('application').urlPrefix();
+
+      await this.uploader.startUpload(
+        [host, 'chapters', id, 'upload'].join('/')
+      );
+
+      this.complete = true;
+      this.transitionToRoute('teach.thumbnail-upload', id);
     } catch (e) {
-      console.log(e);
       this.notify.alert(
-        'Unexpected error encountered and we could not save the content'
+        'We have encountered unexpected error when uploading the H5P content'
       );
     }
   }
