cfd90a6b2d5edc31b6961b1a664e82b2cb8b659f
Brian Marete <bmarete10@gmail.com>
2021-03-30 15:35:58 +0300

    Style login section

--- frontend/app/routes/teach/login.js
+++ frontend/app/routes/teach/login.js
@@ -1,4 +1,21 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class TeachLoginRoute extends Route {
+  @service me;
+  @service SeoTags;
+
+  beforeModel() {
+    if (this.me.user) {
+      this.transitionTo('teach.index');
+    }
+  }
+
+  model() {
+    return this.store.createRecord('user');
+  }
+
+  afterModel() {
+    this.headTags = this.SeoTags.build('Teach - Wikonnect', '/teach/login');
+  }
 }
