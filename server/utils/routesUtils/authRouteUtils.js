089837dc9f93ed98df6175d11012ecd3c675de5a
Brian Marete <bmarete10@gmail.com>
2021-04-08 16:13:27 +0300

    Fix typo in variable naming

--- server/utils/routesUtils/authRouteUtils.js
+++ server/utils/routesUtils/authRouteUtils.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcrypt');
 module.exports = async (ctx, next) => {
-  if (ctx.request.body.auth.newPassword === ctx.request.body.auth.verifyPassword) {
-    const hash = await bcrypt.hash(ctx.request.body.auth.newPassword, 10);
+  if (ctx.request.body.auth.new_password === ctx.request.body.auth.verify_password) {
+    const hash = await bcrypt.hash(ctx.request.body.auth.new_password, 10);
     delete ctx.request.body.auth.newPassword;
     delete ctx.request.body.auth.verifyPassword;
     ctx.request.body.auth.hash = hash;
