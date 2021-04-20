2f4260f04e48f8b15cd7908a5247e8c9b86fe46f
mimidotuser <murage@labskenya.com>
2021-04-20 23:33:31 +0300

    feat: addition of notification migrations, seed file, and model file.
    added a list of classification of events that user can trigger (any missing event can still be added later on)

---
+++ server/db/seeds/dev/09_notifications.js
@@ -0,0 +1,116 @@
+const {faker, seed_number} = require('../_seeds');
+const {eventCodes} = require('./../../../utils/events-classification');
+
+exports.seed = function (knex) {
+  // Deletes ALL existing entries
+  return knex('notifications').del()
+    .then(async () => {
+      const userIds = await knex('users').pluck('id');
+
+      const comments = await knex('comments').select('id', 'chapter_id');
+
+      const ratings = await knex('ratings')
+        .select('id', 'chapter_id', 'reaction')
+        .where('is_deleted', false);
+
+
+      const chapters = await knex('chapters')
+        .select('id', 'approved', 'verified', 'creator_id');
+
+      /**
+             * seed distribution: 1/4 -comments, 1/4 -ratings, 1/4 - approved chapters, 1/4- verified chapters
+             */
+
+      const maxSeed = Math.floor(seed_number / 4);
+
+      //paint the comments (maxSeed only) with chapter creator Id & other notification properties
+      const notificationComments = comments.slice(0, maxSeed) //we only need a portion
+        .map((comment) => {
+          const chapter = chapters.find((p) => p.id === comment.chapter_id);
+          const event_type = comment.parent_id === 'false' ?
+            eventCodes.chapterComment.created :
+            eventCodes.chapterComment.ReplyCreated;
+
+          return {
+            title: 'Your chapter just received a new comment',
+            body: faker.lorem.words(),
+            item_id: comment.id,
+            event_type,
+            model: 'comment',
+            recipient_id: chapter.creator_id,
+            read: faker.random.boolean(),
+            creator_id: faker.random.arrayElement(userIds),
+            created_at: faker.date.past(),
+            updated_at: faker.date.recent(),
+          };
+        });
+
+
+      //paint the ratings notifications with chapter creator Id & other notification properties
+
+      const notificationRatings = ratings.slice(0, maxSeed) //we only need a portion
+        .map((rating) => {
+          const chapter = chapters.find((p) => p.id === rating.chapter_id);
+          const event_type = rating.reaction === 'like' ?
+            eventCodes.rating.positiveCreated :
+            eventCodes.rating.negativeCreated;
+
+          return {
+            title: 'User rated your chapter ' + faker.random.arrayElement(['🎆', '🎉', '✳']),
+            body: faker.lorem.words(),
+            item_id: rating.id,
+            event_type,
+            model: 'rating',
+            recipient_id: chapter.creator_id,
+            read: faker.random.boolean(),
+            creator_id: faker.random.arrayElement(userIds),
+            created_at: faker.date.past(),
+            updated_at: faker.date.recent(),
+          };
+        });
+
+      const approvedChapters = chapters.filter((c) => c.approved === true);
+
+      const approvedChaptersNotifications = approvedChapters.slice(0, maxSeed)
+        .map((chapter) => {
+          return {
+            title: 'Your chapter has been approved',
+            body: faker.lorem.words(),
+            item_id: chapter.id,
+            event_type: eventCodes.chapter.approved,
+            model: 'chapter',
+            recipient_id: chapter.creator_id,
+            read: faker.random.boolean(),
+            metadata: {sendEmail: faker.random.boolean()},
+            creator_id: faker.random.arrayElement(userIds),
+            created_at: faker.date.past(),
+            updated_at: faker.date.recent(),
+          };
+        });
+
+
+      const verifiedChapters = chapters.filter((c) => c.verified === true);
+
+      const verifiedChaptersNotifications = verifiedChapters.slice(0, maxSeed)
+        .map((chapter) => {
+          return {
+            title: 'Your chapter has been verified',
+            body: faker.lorem.words(),
+            item_id: chapter.id,
+            event_type: eventCodes.chapter.verified,
+            model: 'chapter',
+            recipient_id: chapter.creator_id,
+            read: faker.random.boolean(),
+            metadata: {sendEmail: faker.random.boolean()},
+            created_at: faker.date.past(),
+            updated_at: faker.date.recent(),
+          };
+        });
+
+      const notifications = [...notificationComments, ...notificationRatings,
+        ...approvedChaptersNotifications, ...verifiedChaptersNotifications];
+
+      // Inserts seed entries
+      return knex('notifications').insert(notifications);
+    });
+};
