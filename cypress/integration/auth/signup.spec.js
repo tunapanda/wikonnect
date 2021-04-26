15f6151880b0bc08f4407dc69faea49f16803cef
mimidotuser <murage@labskenya.com>
2021-04-26 19:52:20 +0300

    fix: login & signup page integration tests failing

--- cypress/integration/auth/signup.spec.js
+++ cypress/integration/auth/signup.spec.js
@@ -8,20 +8,20 @@ describe('Sign up', () => {
 
     it('should not register user without data', () => {
 
-        cy.get('#email input[type="text"]')
+        cy.get('.signup-form #email input[type="text"]')
             .type('invalid@email')
             .blur()
-            .get('#email .invalid-feedback')
+            .get('.signup-form #email .invalid-feedback')
             .should('be.visible');
 
-        cy.get('.form button[type="submit"]')
+        cy.get('.signup-form button[type="submit"]')
             .click();
 
-        cy.get('#username .invalid-feedback')
+        cy.get('.signup-form #username .invalid-feedback')
             .should('be.visible')
-            .get('#email .invalid-feedback')
+            .get('.signup-form #email .invalid-feedback')
             .should('be.visible')
-            .get('#password .invalid-feedback')
+            .get('.signup-form #password .invalid-feedback')
             .should('be.visible')
             .url()
             .should('include', '/signup');
@@ -33,24 +33,24 @@ describe('Sign up', () => {
         const password = 'dummypassword';
         const email = 'winnie@dummyemail.com';
 
-        cy.get('.signup-form-fields #username input[type="text"]')
+        cy.get('.signup-form .signup-form-fields #username input[type="text"]')
             .type(username)
             .should('have.value', username);
 
-        cy.get('.signup-form-fields #email input[type="text"]')
+        cy.get('.signup-form .signup-form-fields #email input[type="text"]')
             .type(email)
             .should('have.value', email);
 
-        cy.get('.signup-form-fields #password input[type="password"]')
+        cy.get('.signup-form .signup-form-fields #password input[type="password"]')
             .type(password)
             .should('have.value', password);
 
-        cy.get('.signup-form-fields #confirm-password input[type="password"]')
+        cy.get('.signup-form .signup-form-fields #confirm-password input[type="password"]')
             .type(password)
             .should('have.value', password);
 
 
-        cy.get('.form button[type="submit"]')
+        cy.get('.signup-form button[type="submit"]')
             .click()
             .url()
             .should('include', '/upload')
@@ -60,24 +60,24 @@ describe('Sign up', () => {
         cy.fixture('user')
             .then(({username, password, email}) => {
 
-                cy.get('.signup-form-fields #username input[type="text"]')
+                cy.get('.signup-form .signup-form-fields #username input[type="text"]')
                     .type(username)
                     .should('have.value', username);
 
-                cy.get('.signup-form-fields #email input[type="text"]')
+                cy.get('.signup-form .signup-form-fields #email input[type="text"]')
                     .type(email)
                     .should('have.value', email);
 
-                cy.get('.signup-form-fields #password input[type="password"]')
+                cy.get('.signup-form .signup-form-fields #password input[type="password"]')
                     .type(password)
                     .should('have.value', password);
 
-                cy.get('.signup-form-fields #confirm-password input[type="password"]')
+                cy.get('.signup-form .signup-form-fields #confirm-password input[type="password"]')
                     .type(password)
                     .should('have.value', password);
 
 
-                cy.get('.form button[type="submit"]')
+                cy.get('.signup-form button[type="submit"]')
                     .click();
 
                 cy.get('.alert.alert-danger')
@@ -99,7 +99,7 @@ describe('Sign up', () => {
     });
 
     it('should have Google signup call to action', () => {
-        cy.get('form button')
+        cy.get('.signup-form button')
             .contains('Sign up with Google', {matchCase: false})
             .should('be.visible');
     });
