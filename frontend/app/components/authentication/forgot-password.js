e47eb5d099003a79b00cd54cfa6ba89f9a41766d
mimidotuser <murage@labskenya.com>
2021-04-28 17:02:37 +0300

    fix: wait for server response incase of error

--- frontend/app/components/authentication/forgot-password.js
+++ frontend/app/components/authentication/forgot-password.js
@@ -2,8 +2,11 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import fetch from 'fetch';
+import { inject as service } from '@ember/service';
 
 export default class ForgotPasswordComponent extends Component {
+  @service notify;
+
   @tracked email;
   @tracked isInvalid = false;
   @tracked showEmailForm = true;
@@ -19,12 +22,24 @@ export default class ForgotPasswordComponent extends Component {
     e.preventDefault();
 
     const data = { auth: { email: this.email } };
-    await fetch('/api/v1/auth/forgot_password', {
-      method: 'POST',
-      body: JSON.stringify(data),
-      headers: { 'Content-type': 'application/json; charset=UTF-8' },
-    });
+    try {
+      const res = await fetch('/api/v1/auth/forgot_password', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: { 'Content-type': 'application/json; charset=UTF-8' },
+      });
 
-    this.showEmailForm = false;
+      if (res.ok) {
+        this.showEmailForm = false;
+        return;
+      }
+      throw res;
+    } catch (e) {
+      let message = 'Unexpected error encountered';
+      if (e.status === 404) {
+        message = 'User account associated with that email not found';
+      }
+      this.notify.error(message, { closeAfter: 20000 });
+    }
   }
 }
