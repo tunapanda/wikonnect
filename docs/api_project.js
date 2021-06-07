1a13b4fc347e8e51d2be17f0e89a6d83fb9a4d0a
mimidotuser <murage@labskenya.com>
2021-06-07 16:16:37 +0300

    docs: update generated API docs

--- docs/api_project.js
+++ docs/api_project.js
@@ -26,7 +26,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-06-07T12:42:16.801Z",
+    "time": "2021-06-07T13:16:23.533Z",
     "url": "https://apidocjs.com",
     "version": "0.27.1"
   }
