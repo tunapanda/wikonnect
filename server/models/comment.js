285d0e334454653bc91f1f8196fad2dc2bd3e15d
mimidotuser <murage@labskenya.com>
2021-06-10 11:43:50 +0300

    feat(backend): add count users' comment after create hook

--- server/models/comment.js
+++ server/models/comment.js
@@ -1,5 +1,9 @@
+const { raw } = require('objection');
+
 const Model = require('./_model');
 const knex = require('../db/db');
+const {SHooksEventEmitter} = require('../utils/event-emitter');
+const {events} = require('../utils/storage-hooks-events');
 
 class Comment extends Model {
   static get tableName() {
@@ -70,6 +74,27 @@ class Comment extends Model {
         builder.select('id', 'creator_id', 'chapter_id', 'comment', 'createdAt', 'updatedAt');
       }
     };
+  }
+
+  async $afterInsert(queryContext) {
+    await super.$afterInsert(queryContext);
+    
+    if(SHooksEventEmitter.listenerCount(events.user.comment.countOnCreate)>0) {
+      // get total
+      const results =   await Comment.query(queryContext.transaction)
+        .select([
+          raw('COUNT(CASE WHEN parent_id =\'false\' THEN 1 ELSE NULL END) as "totalcomments"'),
+          raw('COUNT(CASE WHEN parent_id <> \'false\' THEN 1 ELSE NULL END) as "totalreplies"')
+        ])
+        .where('creator_id',this.creatorId);
+
+
+      SHooksEventEmitter.emit(events.user.comment.countOnCreate,{
+        totalComments:results[0].totalcomments,
+        totalReplies:results[0].totalreplies,
+        creatorId: this.creatorId
+      });
+    }
   }
 }
 
