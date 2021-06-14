57506d6fd3a40ab4dc8a0207f4461d5caf8ce722
murage <murage@labskenya.com>
2021-06-15 02:19:13 +0300

    [FEATURE] [FRONTEND] User public & private profile view (#735)
    * feat: add profile page viewable public and privately
    
    * fix: user profile card fill available width
    
    * test: fix bare strings lint HBS issues
    
    * fix: remove non-existence user role attribute
    
    * feat(server): add extra user table columns & seed
    
    * fix: add user profile page missing icons
    
    * fix(server): user number column should be dasherized not camerized
    
    * fix(server): user number column should be dasherized not camerized
    
    * feat: add account edit page
    
    * fix(server): return user model on profile image update
    
    * feat: add user profile edit logic
    
    * fix: add missing gender attribute on user model
    
    * feat: update links to edit user profile details & image
    
    * fix: favicon png icon not found
    
    * feat: add un-versioned test files
    
    * fix: only show account details field if available
    
    * feat: chapter author name should link to public user profile
    
    * fix: about me user property is nested on metadata property
    
    * test: add edit profile page integration tests
    
    * fix: user role property missing on user model
    
    * test: add model id on user account fixture
    
    * test: add view user profile details page
    
    * test: fix missing elements/failing tests
    
    Co-authored-by: Moses Okemwa <13944912+mosesokemwa@users.noreply.github.com>

--- frontend/app/models/user.js
+++ frontend/app/models/user.js
@@ -6,7 +6,6 @@ export default class UserModel extends Model {
   @attr() password;
   @attr() firstName;
   @attr() lastName;
-  @attr() aboutMe;
   @attr() lastSeen;
   @attr() tags;
   @attr() inviteCode;
@@ -16,9 +15,20 @@ export default class UserModel extends Model {
   @attr() name;
   @attr() emailVerified;
   @attr() flag;
+  @attr() location;
+  @attr() contactNumber;
+  @attr() gender;
+  @attr() userRoles;
 
   @hasMany('group') groups;
   @hasMany('activity') activities;
   @hasMany('achievement-award') achievementAwards;
   @hasMany('course') enrolledCourses;
+
+  get role() {
+    if (!this.userRoles || !this.userRoles[0]) {
+      return '';
+    }
+    return this.userRoles[0].name;
+  }
 }
