0aa64e0c0311a00d5b911b13db8ff64f147e6a80
mimidotuser <murage@labskenya.com>
2021-06-23 19:15:53 +0300

    feat(server): add user follower model

---
+++ server/models/user-follower.js
@@ -0,0 +1,47 @@
+const Model = require('./_model');
+const knex = require('../db/db');
+
+class UserFollower extends Model {
+  static get tableName() {
+    return 'user_followers';
+  }
+
+  static get jsonSchema() {
+    return {
+      type: 'object',
+      properties: {
+        id: { type: 'string' },
+        userId: { type: 'string' },
+        followingId: { type: 'string' },
+        createdAt: { type: 'string' },
+        updatedAt: { type: 'string' },
+      },
+      required: ['followingId', 'userId'],
+    };
+  }
+
+  static get relationMappings() {
+    return {
+      user: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: __dirname + '/user',
+        join: {
+          from: 'user_followers.user_id',
+          to: 'users.id'
+        }
+      },
+      following: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: __dirname + '/user',
+        join: {
+          from: 'user_followers.following_id',
+          to: 'users.id'
+        }
+      }
+    };
+  }
+
+}
+
+UserFollower.knex(knex);
+module.exports = UserFollower;
