e65f7cdc3fbbd9fdb2c928de0a1dfbf44964a2b1
mimidotuser <murage@labskenya.com>
2021-06-29 21:50:39 +0300

    feat: add user draft courses page logic

---
+++ frontend/app/controllers/course/drafts.js
@@ -0,0 +1,22 @@
+import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
+
+export default class CourseDraftsController extends Controller {
+  @tracked searchTerm;
+
+  get courses() {
+    if (!this.searchTerm) {
+      return this.model;
+    }
+    return this.model.filter((item) => {
+      return (
+        item.name.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+        item.description.toLowerCase().includes(this.searchTerm.toLowerCase())
+      );
+    });
+  }
+
+  get hasDrafts() {
+    return this.model.length > 0;
+  }
+}
--- frontend/app/routes/course/drafts.js
+++ frontend/app/routes/course/drafts.js
@@ -1,3 +1,12 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
-export default class CourseDraftsRoute extends Route {}
+export default class CourseDraftsRoute extends Route {
+  @service me;
+  model() {
+    return this.store.query('course', {
+      status: 'draft',
+      creatorId: this.me.id,
+    });
+  }
+}
