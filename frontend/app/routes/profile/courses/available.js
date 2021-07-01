2e45945325cde6cabe1d2a213779b6c8dc7cb096
mimidotuser <murage@labskenya.com>
2021-07-01 16:42:34 +0300

    feat: add avaialble courses page logic

--- frontend/app/routes/profile/courses/available.js
+++ frontend/app/routes/profile/courses/available.js
@@ -1,3 +1,14 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
-export default class ProfileCoursesAvailableRoute extends Route {}
+export default class ProfileCoursesAvailableRoute extends Route {
+  @service me;
+
+  model() {
+    return this.store.query('course', {
+      include: 'enrollment',
+      enrolledUserId: this.me.id,
+      status: 'published',
+    });
+  }
+}
--- frontend/tests/unit/routes/profile/courses/available-test.js
+++ frontend/tests/unit/routes/profile/courses/available-test.js
@@ -1,10 +1,10 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
-module('Unit | Route | profile/courses/available', function(hooks) {
+module('Unit | Route | profile/courses/available', function (hooks) {
   setupTest(hooks);
 
-  test('it exists', function(assert) {
+  test('it exists', function (assert) {
     let route = this.owner.lookup('route:profile/courses/available');
     assert.ok(route);
   });
