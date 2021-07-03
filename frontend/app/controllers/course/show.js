9e17da4003d1d0002df56332ce48cd4eafba61c4
mimidotuser <murage@labskenya.com>
2021-07-03 15:53:44 +0300

    feat: add popular courses and tags on course view page

--- frontend/app/controllers/course/show.js
+++ frontend/app/controllers/course/show.js
@@ -7,17 +7,30 @@ export default class CourseShowController extends Controller {
   @service notify;
   @service intl;
 
+  get course() {
+    return this.model[0];
+  }
+  get popularCourses() {
+    return this.model[1];
+  }
+  get popularTags() {
+    //sort to ensure long tags are displayed last
+    return this.model[2].toArray().sort((a, b) => {
+      return a.name.length > b.name.length ? 1 : -1;
+    });
+  }
+
   get playlist() {
-    return this.model.get('playlist').sortBy('rank');
+    return this.course.get('playlist').sortBy('rank');
   }
   get isMyCourse() {
-    return this.me.id === this.model.get('creator').get('id');
+    return this.me.id === this.course.get('creator').get('id');
   }
 
   get isEnrolled() {
     return (
-      this.model.courseEnrollments &&
-      this.model.courseEnrollments.findBy('userId', this.me.id)
+      this.course.courseEnrollments &&
+      this.course.courseEnrollments.findBy('userId', this.me.id)
     );
   }
 
@@ -26,11 +39,11 @@ export default class CourseShowController extends Controller {
     let message = this.intl.t('course.show_page.enrolled_successfully');
     const enrollment = this.store.createRecord('course-enrollment', {
       userId: this.me.id,
-      course: this.model,
+      course: this.course,
     });
     try {
       await enrollment.save();
-      await this.model.reload();
+      await this.course.reload();
       this.notify.success(message);
     } catch (e) {
       enrollment.rollbackAttributes();
@@ -44,13 +57,13 @@ export default class CourseShowController extends Controller {
     let message = this.intl.t('course.show_page.disenroll_prompt');
     if (window.confirm(message)) {
       try {
-        const enrollment = this.model.courseEnrollments.findBy(
+        const enrollment = this.course.courseEnrollments.findBy(
           'userId',
           this.me.id
         );
         if (enrollment) {
           await enrollment.destroyRecord();
-          await this.model.reload();
+          await this.course.reload();
           message = this.intl.t('course.show_page.disenroll_successful');
           this.notify.success(message);
         }
--- frontend/app/routes/course/show.js
+++ frontend/app/routes/course/show.js
@@ -5,9 +5,13 @@ export default class CourseShowRoute extends Route {
   @service me;
 
   model({ id }) {
-    return this.store.findRecord('course', id, {
-      include: 'enrollment',
-      enrolledUserId: this.me.id,
-    });
+    return Promise.all([
+      this.store.findRecord('course', id, {
+        include: 'enrollment',
+        enrolledUserId: this.me.id,
+      }),
+      this.store.query('course', { limit: 5, type: 'popular' }),
+      this.store.query('tag', { limit: 5, type: 'popular' }),
+    ]);
   }
 }
