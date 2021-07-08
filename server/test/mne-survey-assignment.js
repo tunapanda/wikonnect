83675d61b6e49618101fd6c360521311fff099a2
mimidotuser <murage@labskenya.com>
2021-07-09 01:27:01 +0300

    test(server): chapter should be approved if published mocha test fix

--- server/test/mne-survey-assignment.js
+++ server/test/mne-survey-assignment.js
@@ -2,16 +2,16 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const chaiJSON = require('chai-json-schema');
 const knex = require('../db/db');
-const triggerModel =require('../models/triggers');
-const userModel =require('../models/user');
-const surveyModel =require('../models/survey');
-const chapterModel =require('../models/chapter');
-const commentModel =require('../models/comment');
-const reactionModel =require('../models/reaction');
-const ratingModel =require('../models/rating');
-const {socketEventEmitter} = require('../utils/event-emitter');
-const {events} = require('../utils/socket-events');
-const {notificationTypes} = require('../utils/notification-constants');
+const triggerModel = require('../models/triggers');
+const userModel = require('../models/user');
+const surveyModel = require('../models/survey');
+const chapterModel = require('../models/chapter');
+const commentModel = require('../models/comment');
+const reactionModel = require('../models/reaction');
+const ratingModel = require('../models/rating');
+const { socketEventEmitter } = require('../utils/event-emitter');
+const { events } = require('../utils/socket-events');
+const { notificationTypes } = require('../utils/notification-constants');
 chai.use(chaiHttp);
 chai.use(chaiJSON);
 chai.should();
@@ -20,27 +20,27 @@ chai.should();
 
 describe('Monitoring & Evaluation Survey Assignment', () => {
 
-  before(async ()=>{
-    
+  before(async () => {
+
     await knex.migrate.rollback(); //reset tables
     await knex.migrate.latest(); // rerun the migrations
     //seed users & triggers only
     await knex.seed.run({
-      specific:'01_users.js'
+      specific: '01_users.js'
     });
     await knex.seed.run({
-      specific:'10_triggers.js'
+      specific: '10_triggers.js'
     });
     //seed survey with predictable data
-    const triggers =await knex(triggerModel.tableName).select('id');
-    const userIds =await knex(userModel.tableName).select('id');
+    const triggers = await knex(triggerModel.tableName).select('id');
+    const userIds = await knex(userModel.tableName).select('id');
     const surveyStatus = {
       published: 'Active',
       unpublished: 'Pending',
       archived: 'Archived',
     };
-    const surveys=[];
-    for (let i = 0; i <triggers.length; i++) {
+    const surveys = [];
+    for (let i = 0; i < triggers.length; i++) {
       surveys.push({
         survey_type: 'mne',
         name: `Test ${i}`,
@@ -58,9 +58,9 @@ describe('Monitoring & Evaluation Survey Assignment', () => {
 
   async function createChapter() {
     await knex(chapterModel.tableName).delete();
-    const userIds =await knex(userModel.tableName).select('id');
+    const userIds = await knex(userModel.tableName).select('id');
 
-    const chapter={
+    const chapter = {
       name: 'This is us on bottom',
       description: 'test',
       approved: false,
@@ -69,93 +69,94 @@ describe('Monitoring & Evaluation Survey Assignment', () => {
     };
     return await chapterModel.query().insertAndFetch(chapter);
   }
-  
-  it('Should assign survey on publishing a chapter', async () =>{
 
-    const chapter= await createChapter();
+  it('Should assign survey on publishing a chapter', async () => {
+
+    const chapter = await createChapter();
 
     await chapterModel.query()
-      .patchAndFetchById(chapter.id,{
-        status:'published'
+      .patchAndFetchById(chapter.id, {
+        status: 'published'
       });
 
-    return new Promise(((resolve,reject) =>{
-      socketEventEmitter.on(events.user.notification.created,(model)=>{
-        if(model.eventType===notificationTypes.chapter.published) {
+    return new Promise(((resolve, reject) => {
+      socketEventEmitter.on(events.user.notification.created, (model) => {
+        if (model.eventType === notificationTypes.chapter.published) {
           resolve(true);
-        }else{
+        } else {
           reject('Invalid notification type');
         }
       });
     }));
 
   });
- 
-  it('Should assign survey on chapter approval', async () =>{
 
-    const chapter= await createChapter();
+  it('Should assign survey on chapter approval', async () => {
+
+    const chapter = await createChapter();
 
     await chapterModel.query()
-      .patchAndFetchById(chapter.id,{
-        approved: true
+      .patchAndFetchById(chapter.id, {
+        approved: true,
+        status: 'published'
       });
 
-    return new Promise(((resolve,reject) =>{
-      socketEventEmitter.on(events.user.notification.created,(model)=>{
-        if(model.eventType===notificationTypes.chapter.approved) {
+    return new Promise(((resolve, reject) => {
+      socketEventEmitter.on(events.user.notification.created, (model) => {
+        if (model.eventType === notificationTypes.chapter.approved) {
           resolve(true);
-        }else{
+        } else {
           reject('Invalid notification type');
         }
       });
     }));
 
   });
-  
-  it('Should assign survey on chapter comment created', async () =>{
 
-    const chapter= await createChapter();
-    const userIds =await knex(userModel.tableName).select('id');
+  it('Should assign survey on chapter comment created', async () => {
+
+    const chapter = await createChapter();
+    const userIds = await knex(userModel.tableName).select('id');
 
     await commentModel.query()
       .insert({
-        chapterId:chapter.id,
-        comment:'Test comment',
+        chapterId: chapter.id,
+        comment: 'Test comment',
         creatorId: userIds[0].id,
       });
 
-    return new Promise(((resolve,reject) =>{
-      socketEventEmitter.on(events.user.notification.created,(model)=>{
-        if(model.eventType===notificationTypes.comment.created) {
+    return new Promise(((resolve, reject) => {
+      socketEventEmitter.on(events.user.notification.created, (model) => {
+        if (model.eventType === notificationTypes.comment.created) {
           resolve(true);
-        }else{
+        } else {
           reject('Invalid notification type');
         }
       });
     }));
 
   });
-  
-  it('Should assign survey on comment reply created', async () =>{
 
-    const chapter= await createChapter();
-    const userIds =await knex(userModel.tableName).select('id');
-    const commentIds =await knex(commentModel.tableName).select('id')
-      .where('parent_id','false');
-    
+  it('Should assign survey on comment reply created', async () => {
+
+    const chapter = await createChapter();
+    const userIds = await knex(userModel.tableName).select('id');
+    const commentIds = await knex(commentModel.tableName).select('id')
+      .where('parent_id', 'false');
+
     await commentModel.query()
       .insert({
-        chapterId:chapter.id,
-        comment:'Test reply',
-        parentId: commentIds[0]?commentIds[0].id:'follow',
+        chapterId: chapter.id,
+        comment: 'Test reply',
+        parentId: commentIds[0] ? commentIds[0].id : 'follow',
         creatorId: userIds[0].id,
       });
 
-    return new Promise(((resolve,reject) =>{
-      socketEventEmitter.on(events.user.notification.created,(model)=>{
-        if(model.eventType===notificationTypes.comment.replied) {
+    return new Promise(((resolve, reject) => {
+      socketEventEmitter.on(events.user.notification.created, (model) => {
+        if (model.eventType === notificationTypes.comment.replied) {
           resolve(true);
-        }else{
+        } else {
           reject('Invalid notification type');
         }
       });
@@ -163,23 +164,23 @@ describe('Monitoring & Evaluation Survey Assignment', () => {
 
   });
 
-  it('Should assign survey on chapter reaction created', async () =>{
+  it('Should assign survey on chapter reaction created', async () => {
 
-    const chapter= await createChapter();
-    const userIds =await knex(userModel.tableName).select('id');
+    const chapter = await createChapter();
+    const userIds = await knex(userModel.tableName).select('id');
 
     await reactionModel.query()
       .insert({
-        chapterId:chapter.id,
+        chapterId: chapter.id,
         reaction: 'like',
         userId: userIds[0].id,
       });
 
-    return new Promise(((resolve,reject) =>{
-      socketEventEmitter.on(events.user.notification.created,(model)=>{
-        if(model.eventType===notificationTypes.reaction.created) {
+    return new Promise(((resolve, reject) => {
+      socketEventEmitter.on(events.user.notification.created, (model) => {
+        if (model.eventType === notificationTypes.reaction.created) {
           resolve(true);
-        }else{
+        } else {
           reject('Invalid notification type');
         }
       });
@@ -187,29 +188,29 @@ describe('Monitoring & Evaluation Survey Assignment', () => {
 
   });
 
-  it('Should assign survey on chapter rating created', async () =>{
+  it('Should assign survey on chapter rating created', async () => {
 
-    const chapter= await createChapter();
-    const userIds =await knex(userModel.tableName).select('id');
+    const chapter = await createChapter();
+    const userIds = await knex(userModel.tableName).select('id');
 
     await ratingModel.query()
       .insert({
-        chapterId:chapter.id,
+        chapterId: chapter.id,
         userId: userIds[0].id,
         averageRating: 10,
         reaction: 'like',
         isDeleted: false,
       });
 
-    return new Promise((resolve =>{
-      socketEventEmitter.on(events.user.notification.created,()=>{
+    return new Promise((resolve => {
+      socketEventEmitter.on(events.user.notification.created, () => {
         resolve(false);
       });
     }));
 
   });
 
-  after(async ()=>{
+  after(async () => {
     await knex.migrate.latest();
     return await knex.seed.run();
   });
