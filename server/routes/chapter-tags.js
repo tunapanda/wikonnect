2600b53c0f000423042cfdcfc5080844ed285aa3
Brian Marete <bmarete10@gmail.com>
2021-07-12 17:13:22 +0300

    Make default tags first in list of tags

--- server/routes/chapter-tags.js
+++ server/routes/chapter-tags.js
@@ -2,11 +2,9 @@ const Router = require('koa-router');
 
 const ChapterModel = require('../models/chapter');
 
-
 const router = new Router({
-  prefix: '/chapter-tags'
+  prefix: '/chapter-tags',
 });
-
 
 /**
  * @api {get} /api/v1/chapter-tags GET all chapter tags
@@ -23,33 +21,55 @@ const router = new Router({
  * @apiSuccessExample {json} Success-Response:
  *     HTTP/1.1 200 OK
  *     {
- *       "chapterTags":[{"name": "Digital Literacy" }]
+ *       'chapterTags':[{'name': 'Digital Literacy' }]
  *      }
  *
  *
  */
 router.get('/', async (ctx) => {
   try {
-
-    const chapters = await ChapterModel.query().select(['tags'])
+    const chapters = await ChapterModel.query()
+      .select(['tags'])
       .whereNotNull('tags')
-      .where('approved',true);
+      .where('approved', true);
 
-    const allTags = chapters.reduce((acc,chapter) => acc.concat(chapter.tags),[]);
+    const allTags = chapters.reduce(
+      (acc, chapter) => acc.concat(chapter.tags),
+      []
+    );
 
-    const chapterTags = [...new Set(allTags)] //remove duplicates
-      .map((t) => {return {name: t};});
+    const defaultTags = [
+      'internet basics',
+      'content creation',
+      'digital wellness',
+      'data protection and privacy',
+      'online safety',
+      'relationships and communications',
+      'news and media literacy',
+      'online working',
+      'online learning',
+      'life skills',
+      'health',
+      'digital financial literacy'
+    ];
+
+    let chapterTags = [...new Set(allTags)] //remove duplicates
+      .filter((tag) => !defaultTags.includes(tag.toLowerCase()));
+
+    chapterTags = [...defaultTags, ...chapterTags] // Default tags should come first
+      .map((tag) => {
+        return { name: tag };
+      });
 
     ctx.status = 200;
-    ctx.body = {chapterTags};
+    ctx.body = { chapterTags };
   } catch (e) {
     if (e.statusCode) {
-      ctx.throw(e.statusCode, null, {errors: [e.message]});
+      ctx.throw(e.statusCode, null, { errors: [e.message] });
     } else {
-      ctx.throw(400, null, {errors: [e.message]});
+      ctx.throw(400, null, { errors: [e.message] });
     }
   }
 });
-
 
 module.exports = router.routes();
