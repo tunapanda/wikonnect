391d03d199b8283dc8415cdee3d60c00f35d7237
mosesokemwa <13944912+mosesokemwa@users.noreply.github.com>
2021-08-02 16:50:22 +0300

    deployment scripts changes
    Signed-off-by: mosesokemwa <13944912+mosesokemwa@users.noreply.github.com>

--- scripts/setup_server.sh
+++ scripts/setup_server.sh
@@ -3,6 +3,7 @@ sudo yarn --cwd ~/wikonnect/server postinstall
 sudo yarn --cwd ~/wikonnect/server db:init
 
 # TODO: Find a long term fix for this
+sleep 5
 rm -rf server/public/h5p/frontend
 mkdir server/public/h5p/frontend
 cp -r frontend/dist/h5p/ server/public/h5p/frontend
