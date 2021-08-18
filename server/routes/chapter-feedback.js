4bc42c9f1040ad3a82edf26ae78f62309e9b064e
Brian Marete <bmarete10@gmail.com>
2021-08-18 11:11:52 +0300

    Feature/content approval (#780)
    * Add content approval route
    
    Added a route to display content awaiting approval in a table
    
    * Add content approval route
    
    Added a route to display content awaiting approval in a table
    
    * Add content approval route
    
    Added a route to display content awaiting approval in a table
    
    * Create chapter feedback table
    
    * Add feedback model and endpoints
    
    * Add user profile to content approval table
    
    * Add approval feature
    
    * Create chapter feedback endpoint
    
    * Add request revision and feedback functionality
    
    * lint fixes and translations
    
    * Add feedback component to display messages
    
    * Change menu icon
    
    * Add status icons
    
    * Show feedback in pop over without reloading the page
    
    * Add sorting
    
    * Add default thumbnail if none
    
    * CSS styling
    
    - Ellipsize description
    - Darken Revisions Requested button
    - Add default placeholder thumbnail
    - Move update time to teacher column
    
    * Add header
    
    * Add search
    
    * Update Translations
    
    * Lint fixes

---
+++ server/routes/chapter-feedback.js
@@ -0,0 +1,49 @@
+const Router = require('koa-router');
+
+const ChapterFeedback = require('../models/chapter_feedback');
+const { requireAuth } = require('../middleware/permController');
+
+const router = new Router({
+  prefix: '/chapterFeedback',
+});
+
+router.get('/', async (ctx) => {
+  try {
+    const chapterFeedback = await ChapterFeedback.query().where(ctx.query);
+
+    ctx.assert(chapterFeedback, 401, 'Something went wrong');
+    ctx.status = 200;
+    ctx.body = { chapterFeedback };
+  } catch (e) {
+    if (e.statusCode) {
+      ctx.throw(e.statusCode, null, { errors: [e] });
+    } else {
+      ctx.throw(400, null, { errors: [e] });
+    }
+    throw e;
+  }
+});
+
+router.post('/', requireAuth, async (ctx) => {
+  
+  const newChapterFeedback = {...ctx.request.body.chapterFeedback};
+
+  let chapterFeedback;
+  try {
+    chapterFeedback = await ChapterFeedback.query().insertAndFetch(
+      newChapterFeedback
+    );
+  } catch (e) {
+    if (e.statusCode) {
+      ctx.throw(e.statusCode, null, { errors: [e.data] });
+    } else {
+      ctx.throw(400, null, { errors: [e.nativeError] });
+    }
+    throw e;
+  }
+  ctx.assert(chapterFeedback, 401, 'Something went wrong');
+  ctx.status = 201;
+  ctx.body = { chapterFeedback };
+});
+
+module.exports = router.routes();
