c6673b648eff22f83ef2e8c896a22174da60ac43
Brian Marete <bmarete10@gmail.com>
2021-08-24 14:50:43 +0300

    lint fixes

--- server/db/migrations/20210824105354_insert_triggers_to_badge_triggers_table.js
+++ server/db/migrations/20210824105354_insert_triggers_to_badge_triggers_table.js
@@ -1,21 +1,21 @@
 exports.up = function (knex) {
-  return knex("badge_triggers").insert([
-    { name: "chapter_publish", description: "a chapter has been published" },
-    { name: "chapter_approved", description: "a chapter has been approved" },
-    { name: "comment_create", description: "a comment has been created" },
-    { name: "comment_reply", description: "a comment has been replied to" },
-    { name: "reaction_create", description: "a user has reacted to a chapter" },
-    { name: "rating_create", description: "a user has rated a chapter" },
+  return knex('badge_triggers').insert([
+    { name: 'chapter_publish', description: 'a chapter has been published' },
+    { name: 'chapter_approved', description: 'a chapter has been approved' },
+    { name: 'comment_create', description: 'a comment has been created' },
+    { name: 'comment_reply', description: 'a comment has been replied to' },
+    { name: 'reaction_create', description: 'a user has reacted to a chapter' },
+    { name: 'rating_create', description: 'a user has rated a chapter' },
   ]);
 };
 
 exports.down = function (knex) {
-  return knex("badge_triggers").whereIn("name", [
-    "chapter_publish",
-    "chapter_approved",
-    "comment_create",
-    "comment_reply",
-    "reaction_create",
-    "rating_create",
+  return knex('badge_triggers').whereIn('name', [
+    'chapter_publish',
+    'chapter_approved',
+    'comment_create',
+    'comment_reply',
+    'reaction_create',
+    'rating_create',
   ]).del();
 };
