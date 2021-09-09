a468e4a3b64a8363d69be35fcd50e88f2e28b242
Brian Marete <bmarete10@gmail.com>
2021-09-09 11:39:56 +0300

    feat: restrict teach routes to only authors of chapter

--- frontend/app/routes/teach/tag.js
+++ frontend/app/routes/teach/tag.js
@@ -3,12 +3,20 @@ import { inject as service } from '@ember/service';
 
 export default class TeachTagRoute extends Route {
   @service me;
+  @service router;
+  @service intl;
+  @service notify;
 
   model(params) {
     return this.store.findRecord('chapter', params.id);
   }
 
-  afterModel() {
+  afterModel(resolvedModel) {
+    if (this.me.id !== resolvedModel.creator.get('id')) {
+      this.notify.error(this.intl.t('teach.index.no_permission'), { closeAfter: 3000 });
+      return this.router.transitionTo('teach');
+    }
+
     return this.store.findAll('tag');
   }
 }
