f21d626dd6594fcbb66f9c680392272e8da6fa7d
murage <murage@labskenya.com>
2021-10-01 16:21:05 +0300

    fix: column reference "metadata" is ambiguous on joined query

--- server/models/user.js
+++ server/models/user.js
@@ -184,19 +184,19 @@ class User extends Model {
           to: 'badges.id'
         }
       }
-    }
+    };
   }
 
   static get modifiers() {
     return {
       selectNameAndId: (builder) => {
-        builder.select('users.id', 'name');
+        builder.select('users.id', 'users.name');
       },
       selectNameAndProfile: (builder) => {
-        builder.select('username', 'profileUri');
+        builder.select('users.username', 'users.profileUri');
       },
-      selectBasicInfo: (query) => {
-        query.select('users.id', 'username', 'metadata', 'profileUri','email');
+      selectBasicInfo: (query) => { // metadata is required to build correct user's name
+        query.select('users.id', 'users.username', 'users.metadata', 'users.profileUri','users.email');
       },
     };
   }
