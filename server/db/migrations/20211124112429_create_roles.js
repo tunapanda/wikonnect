44ad98d5c237816709d127f9a21d196061af355c
Brian Marete <bmarete10@gmail.com>
2022-04-12 12:12:11 +0300

    fix: rewrite create roles migration
    Simplified it and added a check to only insert if there are no roles

--- server/db/migrations/20211124112429_create_roles.js
+++ server/db/migrations/20211124112429_create_roles.js
@@ -1,56 +1,34 @@
-
-exports.up = async function(knex) {
-  await knex.schema.alterTable('group_members', (table) => {
-    table.dropPrimary();
-    table.dropForeign('group_id');
-    table
-        .foreign('group_id')
-        .references('groups.id')
-        .onDelete('CASCADE')
-        .onUpdate('CASCADE');
-    table.text('id').primary().notNullable().defaultTo(knex.raw('next_id()'));
-  });
-
-  return knex('groups').insert([
-    { 
-      id: 'groupAdmin', 
-      name: 'admin', 
-      slug: 'role-admin', 
-      created_at: new Date().toISOString(), 
-      updated_at: new Date().toISOString()
-    },
-    { 
-      id: 'groupModerator', 
-      name: 'moderator', 
-      slug: 'role-moderator', 
-      created_at: new Date().toISOString(), 
-      updated_at: new Date().toISOString()
-    },
-    { 
-      id: 'groupBasic', 
-      name: 'basic', 
-      slug: 'role-basic', 
-      created_at: new Date().toISOString(), 
-      updated_at: new Date().toISOString() 
-    }
-  ]);
+exports.up = async function (knex) {
+  return await knex("groups")
+    .select()
+    .whereIn("name", ["admin", "moderator", "basic"])
+    .then((rows) => {
+      if (rows.length === 0) {
+        return knex("groups").insert([
+          {
+            id: "groupAdmin",
+            name: "admin",
+            slug: "role-admin",
+            created_at: new Date().toISOString(),
+            updated_at: new Date().toISOString(),
+          },
+          {
+            id: "groupModerator",
+            name: "moderator",
+            slug: "role-moderator",
+            created_at: new Date().toISOString(),
+            updated_at: new Date().toISOString(),
+          },
+          {
+            id: "groupBasic",
+            name: "basic",
+            slug: "role-basic",
+            created_at: new Date().toISOString(),
+            updated_at: new Date().toISOString(),
+          },
+        ]);
+      }
+    });
 };
 
-exports.down = async function(knex) {
-  await knex.schema.alterTable('group_members', (table) => {
-    table.setNullable('group_id');
-  });
-
-  await knex('groups').whereIn('name', [
-    'admin',
-    'moderator',
-    'basic',
-  ]).del();
-
-  return knex.schema.alterTable('group_members', (table) => {
-    table.dropPrimary();
-    table.dropColumn('id');
-    table.dropNullable('group_id');
-    table.primary(['user_id', 'group_id']);
-  });
-};
+exports.down = async function (knex) {};
